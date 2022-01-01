5c49a40dcbb7dff799a4c9312879ce277c691558
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-01-02 02:00:14 +0900

    01012021

---
+++ src/programmers/p01012021.java
@@ -0,0 +1,66 @@
+package programmers;
+
+import java.lang.reflect.Array;
+import java.util.*;
+//로또의 최고 순위와 최저 순위
+//01012021
+public class p01012021 {
+    public static void main(String[] args) throws Exception {
+        
+        Solution s = new Solution();
+        int[] a = {44, 1, 0, 0, 31, 25};
+        int[] b = {31, 10, 45, 1, 6, 19};
+        
+        int[] ans = s.solution(a, b);
+
+        System.out.println(ans[0] + ", " + ans[1]);
+    }
+}
+
+class Solution {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        
+        Arrays.sort(lottos);
+        Arrays.sort(win_nums);
+
+        Map map = new HashMap<>();
+        map.put(6, 1);
+        map.put(5, 2);
+        map.put(4, 3);
+        map.put(3, 4);
+        map.put(2, 5);
+        map.put(1, 6);
+        map.put(0, 6);
+
+
+        int correct = 0;
+        int numof_0 = 0;
+
+        for (int a = 0; a < lottos.length; a++) {
+            System.out.println(lottos[a] + "&&&& " + win_nums[a]);
+        }
+
+        for (int i = 0; i < lottos.length; i++) {
+
+            if (lottos[i] == 0) {
+
+                numof_0++;
+            }
+
+            else{
+
+                for (int j : win_nums) {
+
+                    if (lottos[i] == j) {
+                        correct++;
+                    }
+                }
+            }
+        
+        }
+        
+        int[] answer = {(int) map.get(correct + numof_0), (int) map.get(correct)};
+        return answer;
+    }
+
+}
