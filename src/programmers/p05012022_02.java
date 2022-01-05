53c018220631fbea18f0daf68eb537dc0789d100
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-01-06 01:34:01 +0900

    05012022

---
+++ src/programmers/p05012022_02.java
@@ -0,0 +1,48 @@
+package programmers;
+
+import java.util.*;
+
+//없는 숫자 더하기
+//05012022
+
+public class p05012022_02 {
+    public static void main(String[] args) throws Exception {
+        Solution06 s = new Solution06();
+
+        int[] a = {1,2,3,4,6,7,8,0}; 
+
+        System.out.println(s.solution(a));
+    }
+}
+
+class Solution06 {
+
+    public int solution(int[] numbers) {
+
+        HashSet<Integer> numset = new HashSet<Integer>();
+
+        numset.add(0);
+        numset.add(1);
+        numset.add(2);
+        numset.add(3);
+        numset.add(4);
+        numset.add(5);
+        numset.add(6);
+        numset.add(7);
+        numset.add(8);
+        numset.add(9);
+
+        int answer = 0;
+
+        for (int num : numbers) {
+            numset.remove(num);
+        }
+
+        Iterator iter = numset.iterator();
+
+        while(iter.hasNext()) 
+        { answer += (int)iter.next(); }
+
+        return answer;
+    }
+}
