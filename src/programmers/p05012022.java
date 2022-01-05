8a1213673fa54d26159730db4d75eeb393e45fe0
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-01-06 01:10:21 +0900

    05012022

--- src/programmers/p05012022.java
+++ src/programmers/p05012022.java
@@ -21,8 +21,15 @@ class Solution05 {
     public int solution(int[][] board, int[] moves) {
 
         int answer = 0;
-        //ArrayList basket = new ArrayList<Integer>();
-        LinkedList basket = new LinkedList<Integer>();
+        ArrayList basket = new ArrayList<Integer>();
+        //LinkedList basket = new LinkedList<Integer>();
+
+        // 두 방법을 실행했을때, ArrayList를 사용한 방법이 조금 더 빠른 실행시간을 보였다.
+        // 그 이유 :
+        // ArrayList 는 인덱스 조회를 할때, LinkedList 보다 빠른 실행속도를 보여준다.
+        // 그 이유는 리스트 생성시, ArrayList는 배열처럼 값을 특정 장소(공간 = index 번호를 이미 가지고 있음)에 저장하지만,
+        // LinkedList의 경우 인덱스 번호를 함께 저장하지 않기에,
+        // 특정값의 인덱스를 얻기 위해 모든 리스트의 순서를 하나하나씩 매번 탐색해야한다.
 
         for (int i : moves) {
             
@@ -33,12 +40,14 @@ class Solution05 {
                 {
                     basket.add(board[j][i-1]);
 
-                    if (basket.size() > 1 && basket.get(basket.size()-1) == basket.get(basket.size()-2)) {
+                    if (basket.size() > 1 && basket.get(basket.size()-1).equals(basket.get(basket.size()-2))) {
+                        // 위의 if 문의 condition을 살펴보면, 매번 인덱스 확인을 통해 특정값들을 비교하는것을 알 수 있다.
+                        // 따라서 LinkedList 보다 ArrayList가 더 나은 선택임을 알 수 있다.
                         
-                        //basket.remove(basket.size()-1);
-                        //basket.remove(basket.size()-1);
-                        basket.removeLast();
-                        basket.removeLast();
+                        basket.remove(basket.size()-1);
+                        basket.remove(basket.size()-1);
+                        //basket.removeLast();
+                        //basket.removeLast();
 
                         answer += 2;
                     }
@@ -54,3 +63,7 @@ class Solution05 {
         return answer;
     }
 }
+
+//                추가 / 삭제    인덱스 조회
+//  ArrayList       slow         fast
+// LinkedList       fast         slow
