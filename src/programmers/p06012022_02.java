7272f18997d14f75297d33f21fcadb13f796ab81
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-01-07 06:16:09 +0900

    06012022

---
+++ src/programmers/p06012022_02.java
@@ -0,0 +1,30 @@
+package programmers;
+
+import java.util.*;
+
+//내적
+//06012022
+
+public class p06012022_02 {
+    public static void main(String[] args) throws Exception {
+        Solution08 s = new Solution08();
+
+        int[] a = {1,2,3,4};
+        int[] b = {-3,-1,0,2}; 
+
+        System.out.println(s.solution(a,b));
+    }
+}
+
+class Solution08 {
+
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+
+        for (int i = 0; i < b.length; i++) {
+            answer += a[i] * b[i];
+        }
+
+        return answer;
+    }
+}
