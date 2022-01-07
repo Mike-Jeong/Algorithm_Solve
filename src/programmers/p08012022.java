48aba01a3f84bdd6fc2db1837dff8da8742aeed4
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-01-08 05:30:18 +0900

    08012022

---
+++ src/programmers/p08012022.java
@@ -0,0 +1,24 @@
+package programmers;
+
+import java.util.*;
+
+//오픈채팅방
+//08012022
+
+public class p08012022 {
+    public static void main(String[] args) throws Exception {
+        Solution10 s = new Solution10();
+
+        String[] a = {"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"};
+
+        System.out.println(s.solution(a));
+    }
+}
+
+class Solution10 {
+
+    public String[] solution(String[] record) {
+        String[] answer = {};
+        return answer;
+    }
+}
