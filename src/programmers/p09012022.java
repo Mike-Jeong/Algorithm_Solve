45818d1e1e01a2cf8b7f897341ae5d29174f19fc
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-01-09 23:34:37 +0900

    09012022

--- src/programmers/p09012022.java
+++ src/programmers/p09012022.java
@@ -9,8 +9,8 @@ public class p09012022 {
     public static void main(String[] args) throws Exception {
         Solution11 s = new Solution11();
 
-        String[] a = {"leo", "kiki", "eden"};
-        String[] b = {"eden", "kiki"};
+        String[] a = {"marina", "josipa", "nikola", "vinko", "filipa"};
+        String[] b = {"josipa", "filipa", "marina", "nikola"};
 
         System.out.println(s.solution(a, b));
     }
@@ -19,7 +19,31 @@ public class p09012022 {
 class Solution11 {
 
     public String solution(String[] participant, String[] completion) {
+
+        Map<String, Integer> plist = new HashMap<String, Integer>();
         String answer = "";
+
+        for (String string : participant) {
+            if (!plist.containsKey(string)) {
+                plist.put(string, 1);
+            } else {
+                plist.replace(string, plist.get(string), plist.get(string)+1);
+            }       
+        }
+
+        for (String string : completion) {
+
+            plist.replace(string, plist.get(string), plist.get(string)-1);
+        }
+
+        for (String string : plist.keySet()) {
+            if (plist.get(string) != 0) {
+                answer = string;
+            }
+            
+        }
+
         return answer;
+
     }
 }
