755fc3df2cd67238b018f3682852206a2a04f8b8
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-01-10 20:33:09 +0900

    10012022

---
+++ src/programmers/p10012022.java
@@ -0,0 +1,25 @@
+package programmers;
+
+import java.util.*;
+
+//완주하지 못한 선수
+//10012022
+
+public class p10012022 {
+    public static void main(String[] args) throws Exception {
+        Solution11 s = new Solution11();
+
+        String[] a = {"leo", "kiki", "eden"};
+        String[] b = {"eden", "kiki"};
+
+        System.out.println(s.solution(a, b));
+    }
+}
+
+class Solution11 {
+
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        return answer;
+    }
+}
