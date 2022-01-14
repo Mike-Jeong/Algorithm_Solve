99e634df075d98272187590c6fbcf0e67201ce90
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-01-14 17:03:14 +0900

    코드에서 냄시남....ㅠ

--- src/programmers/p14012022.java
+++ src/programmers/p14012022.java
@@ -1,6 +1,7 @@
 package programmers;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 //실패율
 //14012022
@@ -9,15 +10,56 @@ public class p14012022 {
     public static void main(String[] args) throws Exception {
         Solution16 s = new Solution16();
 
-        int[] a = {3,1,2,3};
-        System.out.println(s.solution(a));
+        int a = 5;
+        int[] b = { 2, 1, 2, 6, 2, 4, 3, 3 };
+        System.out.println(s.solution(a, b));
     }
 }
 
 class Solution16 {
 
     public int[] solution(int N, int[] stages) {
-        int[] answer = {};
+
+        int c_num = stages.length;
+        List<Integer> stage = Arrays.stream(stages).boxed().collect(Collectors.toList());
+        Double[] sort_failrate = new Double[N];
+        List<Double> failrate = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+
+            int fail = 0;
+
+            for (int j : stage) {
+                if (j == i+1) {
+                    fail++;
+                }
+            }
+
+            double frate;
+
+            if (fail != 0) {
+                frate = (double) fail / (double)c_num;
+            } else {
+                frate = 0;
+            }
+
+            failrate.add(frate);
+
+            sort_failrate[i] = frate;
+
+            c_num -= fail;
+
+        }
+
+        Arrays.sort(sort_failrate, Collections.reverseOrder());
+
+        int[] answer = new int[N];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = failrate.indexOf(sort_failrate[i]) + 1;
+            failrate.set(answer[i] - 1, -1.0);
+        }
+
         return answer;
     }
 }
