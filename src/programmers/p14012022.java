4db29a9b49a2c2fe4085cb09af186fed29a5f971
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-01-14 15:40:00 +0900

    14012022

---
+++ src/programmers/p14012022.java
@@ -0,0 +1,23 @@
+package programmers;
+
+import java.util.*;
+
+//실패율
+//14012022
+
+public class p14012022 {
+    public static void main(String[] args) throws Exception {
+        Solution16 s = new Solution16();
+
+        int[] a = {3,1,2,3};
+        System.out.println(s.solution(a));
+    }
+}
+
+class Solution16 {
+
+    public int[] solution(int N, int[] stages) {
+        int[] answer = {};
+        return answer;
+    }
+}
