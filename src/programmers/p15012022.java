e96e94dbce19b849d9b6c666c545ebd77f8d01c2
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-01-15 23:50:23 +0900

    15012022

--- src/programmers/p15012022.java
+++ src/programmers/p15012022.java
@@ -10,17 +10,53 @@ public class p15012022 {
     public static void main(String[] args) throws Exception {
         Solution17 s = new Solution17();
 
-        String[] a = {"muzi", "frodo", "apeach", "neo"};
-        String[] b = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
+        String[] a = {"con", "ryan"};
+        String[] b = {"ryan con", "ryan con", "ryan con", "ryan con"};
         int c = 2;
-        System.out.println(s.solution(a, b,c));
+
+        for (int d : s.solution(a, b,c)) {
+            System.out.println(d);
+        }
+        
     }
 }
 
 class Solution17 {
 
     public int[] solution(String[] id_list, String[] report, int k) {
-        int[] answer = {};
+        Map<String,Integer> report_count = new LinkedHashMap<>();
+        Map<String,Integer> mail_count = new LinkedHashMap<>();
+        Map<String,Set<String>> reportlist = new HashMap<>();
+
+
+        for (String string : id_list) {
+            Set<String> report_member = new HashSet<>();
+            report_count.put(string, 0);
+            mail_count.put(string, 0);
+            reportlist.put(string, report_member);
+
+        }
+
+        for (String string : report) {
+            String[] word = string.split(" ");
+            reportlist.get(word[1]).add(word[0]);
+        }
+
+        for (String string : id_list) {
+
+            if (reportlist.get(string).size() >= k) {
+                for (String string2 : reportlist.get(string)) {
+                    mail_count.put(string2, mail_count.get(string2) + 1);
+                }
+            }
+        }
+
+        int[] answer = new int [mail_count.size()];
+        int i = 0;
+        for (String string3 : mail_count.keySet()) {;
+            answer[i] = mail_count.get(string3);
+            i++;
+        }
         return answer;
     }
 }
