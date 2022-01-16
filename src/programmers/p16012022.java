4d0d2e66c18df179bea26fa7e29f52e830b9d4e5
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-01-16 21:02:23 +0900

    16012022

---
+++ src/programmers/p16012022.java
@@ -0,0 +1,27 @@
+package programmers;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+//약수의 개수와 덧셈
+//16012022
+
+public class p16012022 {
+    public static void main(String[] args) throws Exception {
+        Solution18 s = new Solution18();
+
+        int a = 13;
+        int b = 17;
+
+        System.out.println(s.solution(a,b));
+        
+    }
+}
+
+class Solution18 {
+
+    public int solution(int left, int right) {
+        int answer = 0;
+        return answer;
+    }
+}
