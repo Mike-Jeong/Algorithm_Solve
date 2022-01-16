a897af14df994b408f09df57aea8877f238edbdc
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-01-16 20:57:27 +0900

    .

--- src/programmers/p15012022.java
+++ src/programmers/p15012022.java
@@ -10,7 +10,6 @@ public class p15012022 {
     public static void main(String[] args) throws Exception {
         Solution17 s = new Solution17();
 
-<<<<<<< HEAD
         String[] a = {"con", "ryan"};
         String[] b = {"ryan con", "ryan con", "ryan con", "ryan con"};
         int c = 2;
@@ -19,19 +18,12 @@ public class p15012022 {
             System.out.println(d);
         }
         
-=======
-        String[] a = {"muzi", "frodo", "apeach", "neo"};
-        String[] b = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
-        int c = 2;
-        System.out.println(s.solution(a, b,c));
->>>>>>> f2d79175d5a35c08954017657b139d5b1ea4e6b5
     }
 }
 
 class Solution17 {
 
     public int[] solution(String[] id_list, String[] report, int k) {
-<<<<<<< HEAD
         Map<String,Integer> report_count = new LinkedHashMap<>();
         Map<String,Integer> mail_count = new LinkedHashMap<>();
         Map<String,Set<String>> reportlist = new HashMap<>();
@@ -65,9 +57,6 @@ class Solution17 {
             answer[i] = mail_count.get(string3);
             i++;
         }
-=======
-        int[] answer = {};
->>>>>>> f2d79175d5a35c08954017657b139d5b1ea4e6b5
         return answer;
     }
 }
