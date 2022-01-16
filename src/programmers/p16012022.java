a0eac3a10e41e9d91c795a0c1f9c242fdb155c0c
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-01-16 21:14:13 +0900

    16012022

--- src/programmers/p16012022.java
+++ src/programmers/p16012022.java
@@ -22,6 +22,22 @@ class Solution18 {
 
     public int solution(int left, int right) {
         int answer = 0;
+        for (int i = left; i <= right; i++) {
+            
+            int count_divisor = 0;
+            for (int j = 1; j < i; j++) {
+                if (i%j == 0) {
+                    count_divisor++;
+                }
+            }
+
+            if (count_divisor % 2 == 0) {
+                answer -= i;
+                
+            } else {
+                answer += i;
+            }
+        }
         return answer;
     }
 }
