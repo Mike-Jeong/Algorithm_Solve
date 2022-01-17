71e57f2e4640a443ed0d85cf8b1fb91b25012db8
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-01-17 22:04:36 +0900

    17012022

--- src/programmers/p17012022.java
+++ src/programmers/p17012022.java
@@ -1,7 +1,6 @@
 package programmers;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 //3진법 뒤집기
 //17012022
@@ -20,7 +19,57 @@ public class p17012022 {
 class Solution19 {
 
     public int solution(int n) {
-        int answer = 0;
-        return answer;
+
+        Stack<Integer> reverse = new Stack<>();
+
+        while (n>=3) {
+            reverse.add(n % 3);
+            n /= 3;
+        }
+        reverse.add(n);
+
+        int result = 0;
+        int i = 0;
+
+        while(!reverse.empty()){
+            result += reverse.pop() * Math.pow(3, i);
+            i++;
+        }
+
+        return result;
     }
 }
+
+/* public int solution(int n) {
+        int ternary = 0;
+        int m_10 = 1;
+
+        while (n>=3) {
+            ternary += (n % 3) * m_10;
+            n /= 3;
+            m_10 *= 10;
+        }
+
+        ternary += (n * m_10);
+
+        System.out.println(ternary);
+        System.out.println(m_10);
+
+        int reverse_ternary = 0;
+
+        while(ternary!=0){
+            reverse_ternary = reverse_ternary * 10 + ternary % 10;
+            ternary /= 10;
+        }
+
+        int result = 0;
+        int i = 0;
+
+        while(reverse_ternary!=0){
+            result += (reverse_ternary % 10 * Math.pow(3, i));
+            reverse_ternary /= 10;
+            i++;
+        }
+
+        return result;
+    }*/
