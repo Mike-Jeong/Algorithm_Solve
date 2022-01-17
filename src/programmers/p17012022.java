89622eccc690f338a0f1b72fd45fb3130912cfc0
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-01-17 21:11:21 +0900

    17012022

---
+++ src/programmers/p17012022.java
@@ -0,0 +1,26 @@
+package programmers;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+//3진법 뒤집기
+//17012022
+
+public class p17012022 {
+    public static void main(String[] args) throws Exception {
+        Solution19 s = new Solution19();
+
+        int a = 45;
+
+        System.out.println(s.solution(a));
+        
+    }
+}
+
+class Solution19 {
+
+    public int solution(int n) {
+        int answer = 0;
+        return answer;
+    }
+}
