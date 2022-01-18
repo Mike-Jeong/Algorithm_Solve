763416ce54f214a8b43e706d6efcd6c6e320909a
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-01-18 21:55:47 +0900

    18012022

--- src/programmers/p18012022_02.java
+++ src/programmers/p18012022_02.java
@@ -20,7 +20,24 @@ class Solution21 {
 
     public int[] solution(int[] numbers) {
         
-        int[] answer = {};
+        Set<Integer> resultset = new HashSet<>();
+
+        for (int i = 0; i < numbers.length -1; i++) {
+            for (int j = i+1; j < numbers.length; j++) {
+                resultset.add(numbers[i] + numbers[j]);
+            }
+        }
+
+        int[] answer = new int[resultset.size()];
+        int i = 0;
+
+        for (Integer integer : resultset) {
+            answer[i] = integer;
+            i++;
+        }
+
+        Arrays.sort(answer);
+
         return answer;
     }
 }
