e13edcd71d7234e2bd2fd93f470a44f677e65944
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-01-18 21:55:11 +0900

    18012022

---
+++ src/programmers/p18012022_02.java
@@ -0,0 +1,26 @@
+package programmers;
+
+import java.util.*;
+
+//두개 뽑아서 더하기
+//18012022
+
+public class p18012022_02 {
+    public static void main(String[] args) throws Exception {
+        Solution21 s = new Solution21();
+
+        int[] a = {2,1,3,4,1};
+
+        System.out.println(s.solution(a));
+        
+    }
+}
+
+class Solution21 {
+
+    public int[] solution(int[] numbers) {
+        
+        int[] answer = {};
+        return answer;
+    }
+}
