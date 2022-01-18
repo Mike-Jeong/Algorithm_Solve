dccce0dfdc25efc96026c41b449883b11e415934
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-01-18 21:28:48 +0900

    18012022

---
+++ src/programmers/p18012022.java
@@ -0,0 +1,26 @@
+package programmers;
+
+import java.util.*;
+
+//예산
+//18012022
+
+public class p18012022 {
+    public static void main(String[] args) throws Exception {
+        Solution20 s = new Solution20();
+
+        int[] a = {1,3,2,5,4};
+        int b = 9;
+
+        System.out.println(s.solution(a,b));
+        
+    }
+}
+
+class Solution20 {
+
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        return answer;
+    }
+}
