b01201f99519b9a501111b6f36dc4868fa577747
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-01-19 21:06:28 +0900

    19012022

---
+++ src/programmers/p19012022.java
@@ -0,0 +1,25 @@
+package programmers;
+
+import java.util.*;
+
+//문자열 압축
+//19012022
+
+public class p19012022 {
+    public static void main(String[] args) throws Exception {
+        Solution21 s = new Solution21();
+
+        String a = "ababcdcdababcdcd";
+
+        System.out.println(s.solution(a));
+        
+    }
+}
+
+class Solution21 {
+
+    public int solution(String s) {
+        int answer = 0;
+        return answer;
+    }
+}
