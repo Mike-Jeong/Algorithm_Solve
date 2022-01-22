1341f8a2854628531057c2e29492a60e338827bc
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-01-22 23:44:23 +0900

    22012022

--- src/programmers/p22012022.java
+++ src/programmers/p22012022.java
@@ -9,8 +9,8 @@ public class p22012022 {
     public static void main(String[] args) throws Exception {
         Solution23 s = new Solution23();
 
-        int[] a = {1, 2, 3, 9, 10, 12};
-        int b = 7;
+        int[] a = {1, 2, 3};
+        int b = 11;
 
         System.out.println(s.solution(a,b));
 
@@ -19,9 +19,33 @@ public class p22012022 {
 
 class Solution23 {
 
+    int K;
+    int S = 0;
+
     public int solution(int[] scoville, int K) {
-        int[] scovile = scoville;
-        int k = K;
-        return answer;
+        PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
+        for (Integer integer : scoville) {
+            minHeap.add(integer);
+        }
+        this.K = K;
+        return caculate(minHeap);
+    }
+
+    public int caculate(PriorityQueue<Integer> scoville) {
+        
+        if (scoville.peek() >= K) {
+            return S;
+        } else {
+
+            if (scoville.size() <= 1 ) {
+                return -1;
+            }
+            int a = scoville.poll();
+            int b = scoville.poll();
+            int cal = a + (b * 2);
+            scoville.add(cal);
+            S++;
+            return caculate(scoville);
+        }
     }
 }
