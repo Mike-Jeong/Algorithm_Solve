e07b408a07b1f9f51830e3a6422cf2ab1da68534
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-01-22 22:01:23 +0900

    22012022

---
+++ src/programmers/p22012022.java
@@ -0,0 +1,27 @@
+package programmers;
+
+import java.util.*;
+
+//더 맵게
+//22012022
+
+public class p22012022 {
+    public static void main(String[] args) throws Exception {
+        Solution23 s = new Solution23();
+
+        int[] a = {1, 2, 3, 9, 10, 12};
+        int b = 7;
+
+        System.out.println(s.solution(a,b));
+
+    }
+}
+
+class Solution23 {
+
+    public int solution(int[] scoville, int K) {
+        int[] scovile = scoville;
+        int k = K;
+        return answer;
+    }
+}
