070a6bd65059de2ce8c7f45e494bbaa0f6bbcb8c
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-01-23 20:08:41 +0900

    23012022

---
+++ src/programmers/p23012022.java
@@ -0,0 +1,25 @@
+package programmers;
+
+import java.util.*;
+
+//124 나라의 숫자
+//23012022
+
+public class p23012022 {
+    public static void main(String[] args) throws Exception {
+        Solution24 s = new Solution24();
+
+        int a = 7;
+
+        System.out.println(s.solution(a));
+
+    }
+}
+
+class Solution24 {
+
+    public String solution(int n) {
+        String answer = "";
+        return answer;
+    }
+}
