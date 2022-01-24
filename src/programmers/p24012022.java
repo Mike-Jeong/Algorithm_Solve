03f0a6f77db313b0b28c253374a05d390ed91af3
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-01-24 20:05:03 +0900

    24012022

---
+++ src/programmers/p24012022.java
@@ -0,0 +1,26 @@
+package programmers;
+
+import java.util.*;
+
+//2016년
+//24012022
+
+public class p24012022 {
+    public static void main(String[] args) throws Exception {
+        Solution25 s = new Solution25();
+
+        int a = 7;
+        int b = 7;
+
+        System.out.println(s.solution(a,b));
+
+    }
+}
+
+class Solution25 {
+
+    public String solution(int a, int b) {
+        String answer = "";
+        return answer;
+    }
+}
