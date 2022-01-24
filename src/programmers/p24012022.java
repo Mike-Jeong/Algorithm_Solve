a30c7f9a2aaec727fa997bbac0b60d62ca49aa14
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-01-24 22:13:22 +0900

    24012022

--- src/programmers/p24012022.java
+++ src/programmers/p24012022.java
@@ -12,7 +12,7 @@ public class p24012022 {
         int a = 7;
         int b = 7;
 
-        System.out.println(s.solution(a,b));
+        System.out.println(s.solution(a, b));
 
     }
 }
@@ -21,6 +21,17 @@ class Solution25 {
 
     public String solution(int a, int b) {
         String answer = "";
+        String[] day = { "FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU" };
+        int[] date = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        int allDate = 0;
+        for (int i = 0; i < a - 1; i++) {
+            allDate += date[i];
+        }
+
+        allDate += (b - 1);
+        answer = day[allDate % 7];
+
         return answer;
     }
+
 }
