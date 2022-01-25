052ed123aa931420c4e29cd20f62997879f8ab99
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-01-25 20:33:28 +0900

    25012022

---
+++ src/programmers/p25012022.java
@@ -0,0 +1,26 @@
+package programmers;
+
+import java.util.*;
+
+//최소직사각형
+//25012022
+
+public class p25012022 {
+    public static void main(String[] args) throws Exception {
+        Solution26 s = new Solution26();
+
+        int[][] a = {{60, 50}, {30, 70}, {60, 30}, {80, 40}};
+
+
+        System.out.println(s.solution(a));
+
+    }
+}
+
+class Solution26 {
+
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        return answer;
+    }
+}
