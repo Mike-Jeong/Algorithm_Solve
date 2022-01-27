d2f13c7acf59c8cacf5d2e807c38e0ff1dafe4b3
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-01-27 18:31:03 +0900

    27012022

---
+++ src/programmers/p27012022.java
@@ -0,0 +1,28 @@
+package programmers;
+
+import java.util.*;
+
+//부족한 금액 계산하기
+//27012022
+
+public class p27012022 {
+    public static void main(String[] args) throws Exception {
+        Solution28 s = new Solution28();
+        
+        int a = 10;
+        int b = 10;
+        int c = 10;
+
+        System.out.println(s.solution(a,b,c));
+
+    }
+}
+
+class Solution28 {
+
+    public long solution(int price, int money, int count) {
+        long answer = -1;
+
+        return answer;
+    }
+}
