3ddaabb32a1a8226b6b8f0b1a0013d25d6e9cb41
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-01-28 20:38:42 +0900

    28012022

---
+++ src/programmers/p28012022.java
@@ -0,0 +1,27 @@
+package programmers;
+
+import java.util.*;
+
+//비밀지도
+//28012022
+
+public class p28012022 {
+    public static void main(String[] args) throws Exception {
+        Solution29 s = new Solution29();
+
+        int a = 3;
+        int[] b = {9, 20, 28, 18, 11};
+        int[] c = {30, 1, 21, 17, 28};
+
+        System.out.println(s.solution(a,b,c));
+
+    }
+}
+
+class Solution29 {
+
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = {};
+        return answer;
+    }
+}
