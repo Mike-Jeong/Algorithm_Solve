91f3707e0fa0b7d8c0f52c176eec3a501f3ad4cc
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-01-28 21:13:31 +0900

    28012022

--- src/programmers/p28012022.java
+++ src/programmers/p28012022.java
@@ -9,9 +9,9 @@ public class p28012022 {
     public static void main(String[] args) throws Exception {
         Solution29 s = new Solution29();
 
-        int a = 3;
-        int[] b = {9, 20, 28, 18, 11};
-        int[] c = {30, 1, 21, 17, 28};
+        int a = 6;
+        int[] b = {46, 33, 33 ,22, 31, 50};
+        int[] c = {27 ,56, 19, 14, 14, 10};
 
         System.out.println(s.solution(a,b,c));
 
@@ -21,7 +21,14 @@ public class p28012022 {
 class Solution29 {
 
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = {};
+
+        String format = "%" + n + "s";
+        String[] answer = new String[n];
+        for (int i = 0; i < n; i++) {
+            answer[i] = String.format(format, Integer.toBinaryString(arr1[i] | arr2[i]));
+            answer[i] = answer[i].replaceAll("1", "#");
+            answer[i] = answer[i].replaceAll("0", " ");
+        }
         return answer;
     }
 }
