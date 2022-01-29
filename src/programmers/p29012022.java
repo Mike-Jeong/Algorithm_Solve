3a7f871c5ce3b4d02f487e5ef95d724469b18526
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-01-29 20:03:22 +0900

    29012022

---
+++ src/programmers/p29012022.java
@@ -0,0 +1,32 @@
+package programmers;
+
+import java.util.*;
+
+//가운데 글자 가져오기
+//29012022
+
+public class p29012022 {
+    public static void main(String[] args) throws Exception {
+        Solution30 s = new Solution30();
+
+        String a = "qwer";
+
+        System.out.println(s.solution(a));
+
+    }
+}
+
+class Solution30 {
+
+    public String solution(String s) {
+        int size = s.length();
+        String answer = "";
+        if (size % 2 == 0) {
+            answer = s.substring(size/2 -1, size/2 + 1);
+        }
+        else{
+            answer = String.valueOf(s.charAt(size/2));
+        }
+        return answer;
+    }
+}
