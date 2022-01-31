2a69d4485b013d3308d2099871092e805bbac84b
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-01-31 20:23:09 +0900

    31012022

---
+++ src/programmers/p31012022.java
@@ -0,0 +1,26 @@
+package programmers;
+
+import java.util.*;
+
+//다트 게임
+//30012022
+
+public class p31012022 {
+    public static void main(String[] args) throws Exception {
+        Solution32 s = new Solution32();
+        int[] a = { 1, 1, 3, 3, 0, 1, 1 }; // [1,3,0,1]
+        System.out.println(s.solution(a));
+
+    }
+}
+
+class Solution32 {
+
+    public int[] solution(int[] arr) {
+
+        int[] answer = {};
+
+        return answer;
+    }
+
+}
