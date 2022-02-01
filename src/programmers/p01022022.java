659f552e5530c4d8950f76c34b31556477902197
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-02-01 17:40:24 +0900

    01022022

--- src/programmers/p01022022.java
+++ src/programmers/p01022022.java
@@ -8,7 +8,7 @@ import java.util.*;
 public class p01022022 {
     public static void main(String[] args) throws Exception {
         Solution33 s = new Solution33();
-        int[] a = { 1, 1, 3, 3, 0, 1, 1 }; // [1,3,0,1]
+        int[] a = {5, 9, 7, 10}; // [1,3,0,1]
         int b = 5;
         System.out.println(s.solution(a, b));
 
@@ -18,7 +18,31 @@ public class p01022022 {
 class Solution33 {
 
     public int[] solution(int[] arr, int divisor) {
-        int[] answer = {};
+        
+        List<Integer> nums = new LinkedList<>();
+        int[] answer;
+
+        for (Integer integer : arr) {
+            
+            if (integer % divisor == 0) {
+                nums.add(integer);
+            }
+        }
+        
+        if (nums.isEmpty()) {
+            answer = new int[1];
+            answer[0] = -1;
+            return answer;
+        }
+
+        Collections.sort(nums);
+
+        answer = new int[nums.size()];
+
+        for (int i = 0; i < nums.size(); i++) {
+            answer[i] = nums.get(i);
+        }
+        
         return answer;
     }
 }
