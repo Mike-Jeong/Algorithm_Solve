309015b7305d410209433c15d268040485bb9e2b
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-02-01 17:22:33 +0900

    01022022

---
+++ src/programmers/p01022022.java
@@ -0,0 +1,24 @@
+package programmers;
+
+import java.util.*;
+
+//나누어 떨어지는 숫자 배열
+//01022022
+
+public class p01022022 {
+    public static void main(String[] args) throws Exception {
+        Solution33 s = new Solution33();
+        int[] a = { 1, 1, 3, 3, 0, 1, 1 }; // [1,3,0,1]
+        int b = 5;
+        System.out.println(s.solution(a, b));
+
+    }
+}
+
+class Solution33 {
+
+    public int[] solution(int[] arr, int divisor) {
+        int[] answer = {};
+        return answer;
+    }
+}
