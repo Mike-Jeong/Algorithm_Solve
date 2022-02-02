c202b1fc4d7da990285b88808556759fe8786a7a
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-02-02 14:02:05 +0900

    02222022

---
+++ src/programmers/p02022022.java
@@ -0,0 +1,24 @@
+package programmers;
+
+import java.util.*;
+
+//두 정수 사이의 합
+//02022022
+
+public class p02022022 {
+    public static void main(String[] args) throws Exception {
+        Solution34 s = new Solution34();
+        int a = 3; // 12
+        int b = 5;
+        System.out.println(s.solution(a, b));
+
+    }
+}
+
+class Solution34 {
+
+    public long solution(int a, int b) {
+        long answer = 0;
+        return answer;
+    }
+}
