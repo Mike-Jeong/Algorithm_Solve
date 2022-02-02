b8271a2270f9c04821a5dd2b528aae5f20009556
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-02-02 14:08:56 +0900

    02022022

--- src/programmers/p02022022.java
+++ src/programmers/p02022022.java
@@ -19,6 +19,11 @@ class Solution34 {
 
     public long solution(int a, int b) {
         long answer = 0;
+
+        for (int i = Math.min(a, b); i <= Math.max(a, b); i++) {
+            answer += i;
+        }
+        
         return answer;
     }
 }
