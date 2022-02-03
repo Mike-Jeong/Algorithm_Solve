8a99c55a61c04481cdd69f5f87c8466d23a9b437
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-02-03 23:11:17 +0900

    03022022

---
+++ src/programmers/p03022022.java
@@ -0,0 +1,24 @@
+package programmers;
+
+import java.util.*;
+
+//기능개발
+//03022022
+
+public class p03022022 {
+    public static void main(String[] args) throws Exception {
+        Solution35 s = new Solution35();
+
+        int[] a = {93, 30, 55};
+        int[] b = {1, 30, 5};
+
+        System.out.println(s.solution(a,b));
+    }
+}
+
+class Solution35 {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+        return answer;
+    }
+}
