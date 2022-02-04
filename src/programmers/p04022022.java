56fea7780d3b772dbeafc1680ddce25b2ebafbfe
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-02-04 17:35:28 +0900

    04022022

---
+++ src/programmers/p04022022.java
@@ -0,0 +1,24 @@
+package programmers;
+
+import java.util.*;
+
+//타겟 넘버
+//04022022
+
+public class p04022022 {
+    public static void main(String[] args) throws Exception {
+        Solution36 s = new Solution36();
+
+        int[] a = {1, 1, 1, 1, 1};
+        int b = 3;
+
+        System.out.println(s.solution(a, b));
+    }
+}
+
+class Solution36 {
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+        return answer;
+    }
+}
