2a8e1c8d88d1f4369f61c45470b261b61b38878a
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-02-06 20:12:56 +0900

    06022022

---
+++ src/programmers/p06022022.java
@@ -0,0 +1,25 @@
+package programmers;
+
+import java.util.*;
+
+//행렬 테두리 회전하기
+//06022022
+
+public class p06022022 {
+    public static void main(String[] args) throws Exception {
+        Solution38 s = new Solution38();
+
+        String[] a = {"sun", "bed", "car"};
+        int b = 1;
+
+        System.out.println(s.solution(a, b));
+    }
+}
+
+class Solution38 {
+
+    public String[] solution(String[] strings, int n) {
+        String[] answer = {};
+        return answer;
+    }
+}
