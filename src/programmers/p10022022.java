ba45a244255686fac8ad2bc9dbe64d34264e906b
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-02-10 19:01:02 +0900

    10022022

---
+++ src/programmers/p10022022.java
@@ -0,0 +1,24 @@
+package programmers;
+
+import java.util.*;
+
+//문자열 내 p와 y의 개수
+//10022022
+
+public class p10022022 {
+    public static void main(String[] args) throws Exception {
+        Solution42 s = new Solution42();
+
+        String a = "oooo";
+
+        System.out.println(s.solution(a));
+    }
+}
+
+class Solution42 {
+
+    public String solution(String s) {
+        String answer = "";
+        return answer;
+    }
+}
