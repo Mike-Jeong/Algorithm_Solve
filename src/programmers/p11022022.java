68816c9ea7c3e50e6379aade7e5653733ed9753d
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-02-11 17:02:23 +0900

    11022022

---
+++ src/programmers/p11022022.java
@@ -0,0 +1,33 @@
+package programmers;
+
+import java.util.*;
+
+//문자열 다루기 기본
+//11022022
+
+public class p11022022 {
+    public static void main(String[] args) throws Exception {
+        Solution43 s = new Solution43();
+
+        String a = "1e22";
+
+        System.out.println(s.solution(a));
+    }
+}
+
+class Solution43 {
+
+    public boolean solution(String s) {
+
+        if (s.length() == 4 || s.length() == 6) {
+            for (int i = 0; i < s.length(); i++) {
+                if (!Character.isDigit(s.charAt(i))) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        return false;
+    }
+}
