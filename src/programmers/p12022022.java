ad467d4c0d52178d34bc88dd349634063fedfda6
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-02-12 22:22:11 +0900

    12022022

---
+++ src/programmers/p12022022.java
@@ -0,0 +1,24 @@
+package programmers;
+
+import java.util.*;
+
+//서울에서 김서방 찾기
+//12022022
+
+public class p12022022 {
+    public static void main(String[] args) throws Exception {
+        Solution44 s = new Solution44();
+
+        String[] a = {"Jane", "Kim"};
+
+        System.out.println(s.solution(a));
+    }
+}
+
+class Solution44 {
+
+    public String solution(String[] seoul) {
+        String answer = "";
+        return answer;
+    }
+}
