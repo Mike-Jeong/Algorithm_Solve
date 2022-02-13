5d9d28f8680ec1d19186517d556f55815d005604
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-02-13 22:27:27 +0900

    13022022

---
+++ src/programmers/p13022022.java
@@ -0,0 +1,31 @@
+package programmers;
+
+import java.util.*;
+
+//짝지어 제거하기
+//13022022
+
+public class p13022022 {
+    public static void main(String[] args) throws Exception {
+        Solution44 s = new Solution44();
+
+        String a = "baabaa";
+
+        System.out.println(s.solution(a));
+    }
+}
+
+class Solution44 {
+
+    public int solution(String s)
+    {
+        
+
+        return 0;
+    }
+
+    public int recursion(String s){
+
+        return 0;
+    }
+}
