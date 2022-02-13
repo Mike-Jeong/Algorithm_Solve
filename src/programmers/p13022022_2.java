d711506413b87d50abd1a097cc999fbfe56ee4c5
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-02-13 23:16:56 +0900

    13022022

---
+++ src/programmers/p13022022_2.java
@@ -0,0 +1,24 @@
+package programmers;
+
+import java.util.*;
+
+//전화번호 목록
+//13022022
+
+public class p13022022_2 {
+    public static void main(String[] args) throws Exception {
+        Solution46 s = new Solution46();
+
+        String[] a = {"119", "97674223", "1195524421"};
+
+        System.out.println(s.solution(a));
+    }
+}
+
+class Solution46 {
+
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        return answer;
+    }
+}
