c238133da3d9f50307b34ded0294fc0f58ce68d5
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-02-14 23:00:03 +0900

    14022022

---
+++ src/programmers/p14022022.java
@@ -0,0 +1,26 @@
+package programmers;
+
+import java.util.*;
+
+//프린터
+//14022022
+
+public class p14022022 {
+    public static void main(String[] args) throws Exception {
+        Solution47 s = new Solution47();
+
+        int[] a = {2, 1, 3, 2};
+        int b = 2;
+        //1
+
+        System.out.println(s.solution(a,b));
+    }
+}
+
+class Solution47 {
+
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        return answer;
+    }
+}
