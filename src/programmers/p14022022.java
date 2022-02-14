359da772764b21a6770b062605ba60f62ad2c504
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-02-15 00:00:37 +0900

    14022022

--- src/programmers/p14022022.java
+++ src/programmers/p14022022.java
@@ -9,7 +9,7 @@ public class p14022022 {
     public static void main(String[] args) throws Exception {
         Solution47 s = new Solution47();
 
-        int[] a = {2, 1, 3, 2};
+        int[] a = {2,1,3,2};
         int b = 2;
         //1
 
@@ -20,7 +20,28 @@ public class p14022022 {
 class Solution47 {
 
     public int solution(int[] priorities, int location) {
-        int answer = 0;
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>(Collections.reverseOrder());
+        Queue<Integer> order = new LinkedList<>();
+        List<Integer> f_order = new ArrayList<>();
+        
+        for (int i = 0; i < priorities.length; i++) {
+            queue.add(priorities[i]);
+            order.add(i);
+        }
+
+        while (!order.isEmpty()) {
+            int index = order.poll();
+            int target = priorities[index];
+            if (queue.peek() == target) {
+                queue.poll();
+                f_order.add(index);
+            }
+            else{
+                order.add(index);
+            }
+        }
+
+        int answer = f_order.indexOf(location) + 1;
         return answer;
     }
 }
