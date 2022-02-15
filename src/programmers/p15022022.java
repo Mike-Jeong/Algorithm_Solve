94ff7cf9c26a5c4c7371416e2cabb68d13fee8a0
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-02-15 22:22:11 +0900

    15022022

---
+++ src/programmers/p15022022.java
@@ -0,0 +1,25 @@
+package programmers;
+
+import java.util.*;
+
+//소수찾기
+//15022022
+
+public class p15022022 {
+    public static void main(String[] args) throws Exception {
+        Solution48 s = new Solution48();
+
+        int a = 10;
+        //4
+
+        System.out.println(s.solution(a));
+    }
+}
+
+class Solution48 {
+
+    public int solution(int n) {
+        int answer = 0;
+        return answer;
+    }
+}
