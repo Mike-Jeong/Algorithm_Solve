fc9412754aa83bf6f30fa7207d082ec96bca4162
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-02-15 22:40:11 +0900

    15022022

--- src/programmers/p15022022.java
+++ src/programmers/p15022022.java
@@ -20,6 +20,25 @@ class Solution48 {
 
     public int solution(int n) {
         int answer = 0;
+        int arr[] = new int[n+1];
+        arr[0] = 0; 
+        arr[1] = 0;
+        
+        for (int i = 2 ; i <= n; i++) { arr[i] = i; }
+        
+        for (int i = 2 ; i <= (int)Math.sqrt(n) ; i++) {
+
+            if(arr[i] == 0) continue;
+
+            for (int j = 2 * i ; j <= n; j+=i) {
+                arr[j] = 0;
+            }
+        }
+
+        for (int i = 0; i <= n ; i++ ) {
+            if (arr[i] != 0) answer++;
+        }
+
         return answer;
     }
 }
