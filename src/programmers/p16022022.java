59b8438cef1b8a4d9792e200732970742eb57127
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-02-16 23:46:23 +0900

    16022022

---
+++ src/programmers/p16022022.java
@@ -0,0 +1,34 @@
+package programmers;
+
+import java.util.*;
+
+//수박수박수박수박수박수?
+//16022022
+
+public class p16022022 {
+    public static void main(String[] args) throws Exception {
+        Solution49 s = new Solution49();
+
+        int a = 3;
+        //4
+
+        System.out.println(s.solution(a));
+    }
+}
+
+class Solution49 {
+
+    public String solution(int n) {
+        StringBuilder ans = new StringBuilder("");
+
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || i % 2 == 0) {
+                ans.append("수");
+            }
+            else{ans.append("박");}
+        }
+
+        String answer = ans.toString();
+        return answer;
+    }
+}
