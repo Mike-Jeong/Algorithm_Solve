7ce1b3d1ba9c97a20e5cc97d2a83c78a4384873c
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-02-17 22:29:44 +0900

    17022022

---
+++ src/programmers/p17022022.java
@@ -0,0 +1,25 @@
+package programmers;
+
+import java.util.*;
+
+//메뉴 리뉴얼
+//17022022
+
+public class p17022022 {
+    public static void main(String[] args) throws Exception {
+        Solution50 s = new Solution50();
+
+        String[] a = {"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"};
+        int[] b = {2,3,4};
+
+        System.out.println(s.solution(a, b));
+    }
+}
+
+class Solution50 {
+
+    public String[] solution(String[] orders, int[] course) {
+        String[] answer = {};
+        return answer;
+    }
+}
