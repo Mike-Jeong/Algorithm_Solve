b5e334e9f9462a3c9f2c50f0f72bd8c920b9913f
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-02-18 17:14:07 +0900

    18022022

--- src/programmers/p18022022.java
+++ src/programmers/p18022022.java
@@ -9,17 +9,16 @@ public class p18022022 {
     public static void main(String[] args) throws Exception {
         Solution51 s = new Solution51();
 
-        String[] a = { "XYZ", "XWY", "WXA" };
-        int[] b = { 2, 3, 4 };
+        String a = "1234";
 
-        System.out.println(s.solution(a, b));
+        System.out.println(s.solution(a));
     }
 }
 
 class Solution51 {
 
     public int solution(String s) {
-        int answer = 0;
+        int answer = Integer.parseInt(s);
         return answer;
     }
 }
