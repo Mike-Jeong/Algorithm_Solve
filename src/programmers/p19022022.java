724f4401c94bcd8e00766e20eccb7d7d461906ce
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-02-19 21:01:34 +0900

    19022022

---
+++ src/programmers/p19022022.java
@@ -0,0 +1,25 @@
+package programmers;
+
+import java.util.*;
+
+//시저암호
+//19022022
+
+public class p19022022 {
+    public static void main(String[] args) throws Exception {
+        Solution52 s = new Solution52();
+
+        String a = "AB";
+        int b = 1;
+
+        System.out.println(s.solution(a,b));
+    }
+}
+
+class Solution52 {
+
+    public String solution(String s, int n) {
+        String answer = "";
+        return answer;
+    }
+}
