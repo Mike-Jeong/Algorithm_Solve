cf7d341b245ba362107268fac763bec6f3bda1c0
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-02-19 21:30:34 +0900

    19022022

--- src/programmers/p19022022.java
+++ src/programmers/p19022022.java
@@ -12,14 +12,38 @@ public class p19022022 {
         String a = "AB";
         int b = 1;
 
-        System.out.println(s.solution(a,b));
+        System.out.println(s.solution(a, b));
     }
 }
 
 class Solution52 {
 
     public String solution(String s, int n) {
-        String answer = "";
+
+        StringBuilder sb = new StringBuilder("");
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i); 
+
+            if (ch == ' ') {
+                sb.append(ch);
+                continue; 
+            }
+            
+            if(Character.isLowerCase(ch)) { 
+              
+                ch = (char) ((ch - 'a' + n) % 26 + 'a'); 
+            } 
+            
+            else {
+                ch = (char) ((ch - 'A' + n) % 26 + 'A'); 
+            }
+
+            sb.append(ch);    
+        }
+
+        String answer = sb.toString();
         return answer;
     }
 }
