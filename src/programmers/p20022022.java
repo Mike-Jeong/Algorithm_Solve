28970cda6fc42a5c328112180247d74defd6ee8a
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-02-20 21:59:03 +0900

    20022022

--- src/programmers/p20022022.java
+++ src/programmers/p20022022.java
@@ -19,6 +19,11 @@ class Solution53 {
 
     public int solution(int n) {
         int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                answer  += i;
+            }
+        }
         return answer;
     }
 }
