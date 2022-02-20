cbd6566ba6d66173763bb6ebe84c34ea4ebf73c0
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-02-20 21:56:42 +0900

    20022022

---
+++ src/programmers/p20022022.java
@@ -0,0 +1,24 @@
+package programmers;
+
+import java.util.*;
+
+//약수의 합
+//20022022
+
+public class p20022022 {
+    public static void main(String[] args) throws Exception {
+        Solution53 s = new Solution53();
+
+        int a = 12;
+
+        System.out.println(s.solution(a));
+    }
+}
+
+class Solution53 {
+
+    public int solution(int n) {
+        int answer = 0;
+        return answer;
+    }
+}
