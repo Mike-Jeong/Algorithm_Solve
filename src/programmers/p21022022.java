ac4998291015d82d19f11ff10dff3e133ebce992
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-02-21 22:02:57 +0900

    21022022

---
+++ src/programmers/p21022022.java
@@ -0,0 +1,24 @@
+package programmers;
+
+import java.util.*;
+
+//튜플
+//21022022
+
+public class p21022022 {
+    public static void main(String[] args) throws Exception {
+        Solution54 s = new Solution54();
+
+        String a = "{{2},{2,1},{2,1,3},{2,1,3,4}}";
+
+        System.out.println(s.solution(a));
+    }
+}
+
+class Solution54 {
+
+    public int[] solution(String s) {
+        int[] answer = {};
+        return answer;
+    }
+}
