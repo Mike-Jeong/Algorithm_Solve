369b3aabd234ca85ec742c5c0eb0b0b838a88958
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-02-21 23:15:23 +0900

    21022022

--- src/programmers/p21022022.java
+++ src/programmers/p21022022.java
@@ -9,7 +9,7 @@ public class p21022022 {
     public static void main(String[] args) throws Exception {
         Solution54 s = new Solution54();
 
-        String a = "{{2},{2,1},{2,1,3},{2,1,3,4}}";
+        String a = "{{20,111},{111}}";
 
         System.out.println(s.solution(a));
     }
@@ -18,7 +18,43 @@ public class p21022022 {
 class Solution54 {
 
     public int[] solution(String s) {
-        int[] answer = {};
+        Map<Integer, Set<Integer>> sequence = new HashMap<>();
+        Set<Integer> nums = new HashSet<>();
+
+        s = s.replace("{", "").replace("}", " ");
+        String[] a = s.split(" ");
+
+        for (String string : a) {
+
+            String[] b = string.split(",");
+
+            Set<Integer> tuple = new HashSet<>();
+
+            for (String string2 : b) {
+
+                if (!string2.isEmpty()) {
+                    
+                    tuple.add(Integer.parseInt(string2));
+                }
+            }
+
+            sequence.put(tuple.size(), tuple);
+        }
+
+        int[] answer = new int[sequence.size()];
+
+        for (int i = 0; i < answer.length; i++) {
+
+            for (int num : sequence.get(i + 1)) {
+                
+                if (!nums.contains(num)) {
+                    answer[i] = num;
+                    nums.add(num);
+                    break;
+                }
+            }
+        }
+        
         return answer;
     }
 }
