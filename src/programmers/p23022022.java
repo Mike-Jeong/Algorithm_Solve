0901b70e6bb0e7fd8343aec08f28df09da158353
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-02-23 20:04:23 +0900

    23022022

---
+++ src/programmers/p23022022.java
@@ -0,0 +1,24 @@
+package programmers;
+
+import java.util.*;
+
+//가장 큰 수
+//23022022
+
+public class p23022022 {
+    public static void main(String[] args) throws Exception {
+        Solution56 s = new Solution56();
+
+        int[] a = {6, 10, 2};
+
+        System.out.println(s.solution(a));
+    }
+}
+
+class Solution56 {
+
+    public String solution(int[] numbers) {
+        String answer = "";
+        return answer;
+    }
+}
