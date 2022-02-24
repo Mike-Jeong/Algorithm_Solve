73319238b98a208870d4685613d0069e905df5b6
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-02-24 20:32:23 +0900

    24022022

---
+++ src/programmers/p24022022.java
@@ -0,0 +1,24 @@
+package programmers;
+
+import java.util.*;
+
+//가장 큰 수
+//24022022
+
+public class p24022022 {
+    public static void main(String[] args) throws Exception {
+        Solution57 s = new Solution57();
+
+        String a = "17";
+
+        System.out.println(s.solution(a));
+    }
+}
+
+class Solution57 {
+
+    public int solution(String numbers) {
+        int answer = 0;
+        return answer;
+    }
+}
