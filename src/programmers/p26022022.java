4296e4a2a2a8a855378e2c3f0750263c40563677
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-02-26 07:15:02 +0900

    26022022

---
+++ src/programmers/p26022022.java
@@ -0,0 +1,33 @@
+package programmers;
+
+import java.util.*;
+
+//큰 수 만들기
+//26022022
+
+public class p26022022 {
+    public static void main(String[] args) throws Exception {
+
+        Solution59 s = new Solution59();
+
+        String a = "1924";
+        int b = 2;
+
+        System.out.println(s.solution(a,b));
+    }
+}
+
+class Solution59 {
+
+    public String solution(String number, int k) {
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for (int i = 0; i < number.length(); i++) {
+            priorityQueue.add(e)
+        }
+
+        String answer = "";
+        return answer;
+    }
+}
