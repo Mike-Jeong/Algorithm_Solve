f70180305f4532739e46ae4957b1c2d3b48a6262
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-02-26 08:17:22 +0900

    26022022

---
+++ src/programmers/p27022022.java
@@ -0,0 +1,36 @@
+package programmers;
+
+import java.util.*;
+
+//H-Index
+//27022022
+
+public class p27022022 {
+    public static void main(String[] args) throws Exception {
+
+        Solution60 s = new Solution60();
+
+        int[] a = {3, 0, 6, 1, 5};
+
+        System.out.println(s.solution(a));
+    }
+}
+
+class Solution60 {
+
+    public int solution(int[] citations) {
+        Integer[] intArray = new Integer[citations.length];
+
+        for(int i = 0; i < citations.length; i++)
+            intArray[i] = citations[i];
+
+        Arrays.sort(intArray, Collections.reverseOrder());
+
+        for(int i = 0; i < intArray.length; i++) {
+            if(intArray[i] <= i+1)
+                return i;
+        }
+
+        return intArray.length;
+    }
+}
