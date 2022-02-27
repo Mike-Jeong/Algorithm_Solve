d670eb6da40726584f774f021513d4ba41ffe320
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-02-28 08:19:32 +0900

    26022022

--- src/programmers/p26022022.java
+++ src/programmers/p26022022.java
@@ -10,8 +10,8 @@ public class p26022022 {
 
         Solution59 s = new Solution59();
 
-        String a = "1924";
-        int b = 2;
+        String a = "4177252841";
+        int b = 4;
 
         System.out.println(s.solution(a,b));
     }
@@ -21,13 +21,44 @@ class Solution59 {
 
     public String solution(String number, int k) {
 
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
-        
-        for (int i = 0; i < number.length(); i++) {
-            priorityQueue.add(e)
+        StringBuilder sb = new StringBuilder();
+        int index = 0;
+        int max = 0;
+        for(int i=0; i<number.length() - k; i++) {
+            max = 0;
+            for(int j = index; j<= k+i; j++) {
+                if(max < number.charAt(j)-'0') {
+                    max = number.charAt(j)-'0';
+                    index = j+1;
+                }
+            }
+            sb.append(max);
         }
-
-        String answer = "";
-        return answer;
+        return sb.toString(); // 인터넷 퍼옴
     }
 }
+
+/*
+public String solution(String number, int k) {
+
+        int count = 0;
+        int i = 0;
+
+        while (count < k) {
+           
+            int current = Character.getNumericValue(number.charAt(i));
+            int next = Character.getNumericValue(number.charAt(i+1));
+
+            if (current < next) {
+                number = number.replaceFirst(String.valueOf(current), "");
+                i = 0;
+                count ++;
+            }
+            else{
+                i++;
+            }
+        }
+        return number;
+    }
+ */
+// 시간초과, 런타임 에러......
