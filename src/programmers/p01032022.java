d0590b2cce1b493b01d1dff63af264a9fbfc6440
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-01 07:05:22 +0900

    01032022

--- src/programmers/p01032022.java
+++ src/programmers/p01032022.java
@@ -20,11 +20,16 @@ public class p01032022 {
 class Solution62 {
 
     public int[][] solution(int[][] arr1, int[][] arr2) {
+
         int[][] answer = new int[arr1.length][arr2[0].length];
 
-        for (int i = 0; i < answer.length; i++) {
-           for (int j = 0; j < answer[0].length; j++) {
-
+        for (int i = 0; i < arr1.length; i++) {
+           for (int j = 0; j < arr2[0].length; j++) {
+               int sum = 0;
+               for (int l = 0; l < arr1[0].length; l++) {
+                   sum += (arr1[i][l] * arr2[l][j]);
+               }
+               answer[i][j] = sum;
            }
         }
 
