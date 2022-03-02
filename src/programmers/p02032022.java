8655df2bcf1d54500c8254c62a7acb8d8cacca61
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-02 13:46:16 +0900

    02032022

--- src/programmers/p02032022.java
+++ src/programmers/p02032022.java
@@ -19,8 +19,35 @@ public class p02032022 {
 
 class Solution63 {
 
+    int cover;
+    int inside;
+
     public int[] solution(int brown, int yellow) {
-        int[] answer = {};
+        cover = brown;
+        inside = yellow;
+        int num_tile = yellow + brown;
+        int[] answer = new int[2];
+
+        for (int i = 2; i < num_tile / 2; i++) {
+            if (num_tile % i == 0) {
+                if (check(i, num_tile/i)) {
+                    answer[0] = Math.max(i, num_tile/i);
+                    answer[1] = Math.min(i, num_tile/i);
+                    break;
+                }
+            }
+        }
         return answer;
+    }
+
+    public boolean check(int width, int height){
+
+        int out = 2*(width + height) - 4;
+        int in = (width - 2) * (height - 2);
+        if (out == cover && in == inside) {
+            return true;
+        }
+
+        return false;
     }
 }
