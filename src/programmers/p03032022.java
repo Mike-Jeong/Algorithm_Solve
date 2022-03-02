0ea94d6ec426f8ad1c6feffb9d008794167f3aca
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-03 06:28:23 +0900

    03032022

---
+++ src/programmers/p03032022.java
@@ -0,0 +1,25 @@
+package programmers;
+
+import java.util.*;
+
+//숫자의 표현
+//03032022
+
+public class p03032022 {
+    public static void main(String[] args) throws Exception {
+
+        Solution64 s = new Solution64();
+
+        int a = 10;
+
+        System.out.println(s.solution(a));
+    }
+}
+
+class Solution64 {
+
+    public int solution(int n) {
+        int answer = 0;
+        return answer;
+    }
+}
