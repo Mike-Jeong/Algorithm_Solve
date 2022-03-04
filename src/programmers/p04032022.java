e11e9ac00be7c9a9697ce62396b2e561a6060bd0
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-04 11:37:09 +0900

    04032022

--- src/programmers/p04032022.java
+++ src/programmers/p04032022.java
@@ -19,7 +19,24 @@ public class p04032022 {
 class Solution65 {
 
     public int[] solution(int[] prices) {
-        int[] answer = {};
+
+        int[] answer = new int[prices.length];
+
+        for (int i = 0; i < prices.length; i++) {
+
+            answer[i] = 0;
+
+            for (int j = i + 1; j < prices.length; j++) {
+
+                answer[i]++;
+
+                if (prices[i] > prices[j]) {
+                    break;
+                }
+                
+            }
+        }
+        
         return answer;
     }
 }
