1e176cc0a5d9304e8f18441e7cf2c0c9bf53c2a7
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-05 13:44:21 +0900

    05032022

---
+++ src/programmers/p05032022.java
@@ -0,0 +1,29 @@
+package programmers;
+
+import java.util.*;
+
+//주식가격
+//05032022
+
+public class p05032022 {
+    public static void main(String[] args) throws Exception {
+
+        Solution66 s = new Solution66();
+
+        int[][] a = {{1,2,3,5},{5,6,7,8},{4,3,2,1}};
+
+        System.out.println(s.solution(a));
+    }
+}
+
+class Solution66 {
+
+    int solution(int[][] land) {
+        int answer = 0;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
