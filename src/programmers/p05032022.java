00a703756e85e130c663d984e9b9004b460b161f
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-05 16:05:46 +0900

    05032022

--- src/programmers/p05032022.java
+++ src/programmers/p05032022.java
@@ -19,7 +19,31 @@ public class p05032022 {
 class Solution66 {
 
     public int solution(String[][] clothes) {
-        int answer = 0;
-        return answer;
+
+        Map<String,Integer> set = new HashMap<>();
+
+        for (String[] strings : clothes) {
+            
+            if (set.containsKey(strings[1])) {
+                set.replace(strings[1], set.get(strings[1]), set.get(strings[1]) + 1);
+            }
+            else{
+                set.put(strings[1], 1);
+            }
+        }
+
+        int count = 0;
+        int combi = 1;
+
+        for (String strings : set.keySet()) {
+            count += set.get(strings);
+            combi *= set.get(strings) + 1;
+        }
+
+        if (set.size() != 1) {
+            return combi - 1;
+        } else {
+            return count;
+        }
     }
 }
