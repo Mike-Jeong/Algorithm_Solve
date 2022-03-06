900c9de261004acc0be9455dccb9df48d599c7cd
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-06 22:10:33 +0900

    06032022

--- src/programmers/p06032022.java
+++ src/programmers/p06032022.java
@@ -12,7 +12,7 @@ public class p06032022 {
 
         int a = 2;
         int b = 10;
-        int[] c = {7,4,5,6};
+        int[] c = { 7, 4, 5, 6 };
 
         System.out.println(s.solution(a, b, c));
     }
@@ -21,7 +21,37 @@ public class p06032022 {
 class Solution67 {
 
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int answer = 0;
+
+        Queue<Integer> queue = new LinkedList<>();
+        Queue<Integer> onbridge = new LinkedList<>();
+        int count = 0;
+        int current_weight = 0;
+
+        for (Integer integer : truck_weights) {
+            queue.add(integer);
+        }
+
+        for (int i = 0; i < bridge_length; i++) {
+            onbridge.add(0);
+        }
+
+        while (!queue.isEmpty()) {
+
+            count++;
+
+            current_weight -= onbridge.poll();
+
+            if (queue.peek() + current_weight <= weight) {
+                current_weight += queue.peek();
+                onbridge.add(queue.poll());
+
+            } else {
+                onbridge.add(0);
+            }
+
+        }
+
+        int answer = count + bridge_length;
         return answer;
     }
 }
