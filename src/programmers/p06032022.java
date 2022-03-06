7f41f4a69910408c42e4968219f715de07097393
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-06 21:49:49 +0900

    06032022

---
+++ src/programmers/p06032022.java
@@ -0,0 +1,27 @@
+package programmers;
+
+import java.util.*;
+
+//다리를 지나는 트럭
+//06032022
+
+public class p06032022 {
+    public static void main(String[] args) throws Exception {
+
+        Solution67 s = new Solution67();
+
+        int a = 2;
+        int b = 10;
+        int[] c = {7,4,5,6};
+
+        System.out.println(s.solution(a, b, c));
+    }
+}
+
+class Solution67 {
+
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        return answer;
+    }
+}
