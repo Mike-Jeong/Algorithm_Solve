7dd3e0d690918fd459773cce96b372a457ee787e
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-07 22:34:06 +0900

    07032022

---
+++ src/programmers/p07032022.java
@@ -0,0 +1,31 @@
+package programmers;
+
+import java.util.*;
+
+//다리를 지나는 트럭
+//07032022
+
+public class p07032022 {
+    public static void main(String[] args) throws Exception {
+
+        Solution68 s = new Solution68();
+
+        int a = 5;
+        int[][] b = { { 1, 2, 1 }, { 2, 3, 3 }, { 5, 2, 2 }, { 1, 4, 2 }, { 5, 3, 1 }, { 5, 4, 2 } };
+        int c = 3;
+
+        System.out.println(s.solution(a, b, c));
+    }
+}
+
+class Solution68 {
+
+    public int solution(int N, int[][] road, int K) {
+        int answer = 0;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
