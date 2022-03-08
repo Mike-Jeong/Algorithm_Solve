cae6ac5efa81190a30d9f635cf3510a444c4700d
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-08 21:02:19 +0900

    08032022

---
+++ src/programmers/p08032022.java
@@ -0,0 +1,27 @@
+package programmers;
+
+import java.util.*;
+
+//구명보트
+//08032022
+
+public class p08032022 {
+    public static void main(String[] args) throws Exception {
+
+        Solution69 s = new Solution69();
+
+        int[] a = {70,50,80,50};
+        int b = 100;
+
+        s.solution(a, b);
+        System.out.println();
+    }
+}
+
+class Solution69 {
+
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        return answer;
+    }
+}
