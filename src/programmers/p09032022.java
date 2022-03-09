b8e0e419c4cf5b53036acab4fd0663eaa08dd5bd
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-09 18:02:00 +0900

    09032022

---
+++ src/programmers/p09032022.java
@@ -0,0 +1,30 @@
+package programmers;
+
+import java.util.*;
+
+//영어 끝말잇기
+//09032022
+
+public class p09032022 {
+    public static void main(String[] args) throws Exception {
+
+        Solution70 s = new Solution70();
+
+        int a = 3;
+        String[] b = {"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"};
+
+        System.out.println(s.solution(a, b));
+    }
+}
+
+class Solution70 {
+
+    public int[] solution(int n, String[] words) {
+        int[] answer = {};
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다. 
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
