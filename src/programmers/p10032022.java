e9e6202c63b37e1b569554c39b282ec51a9f3679
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-10 23:00:07 +0900

    10032022

---
+++ src/programmers/p10032022.java
@@ -0,0 +1,31 @@
+package programmers;
+
+import java.util.*;
+
+//예상 대진표
+//10032022
+
+public class p10032022 {
+    public static void main(String[] args) throws Exception {
+
+        Solution71 s = new Solution71();
+
+        int a = 8;
+        int b = 4;
+        int c = 7;
+
+        System.out.println(s.solution(a, b, c));
+    }
+}
+
+class Solution71 {
+
+    public int solution(int n, int a, int b) {
+        int answer = 0;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
