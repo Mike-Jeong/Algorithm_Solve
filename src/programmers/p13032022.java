0da43e70f657aafcd89de5c3c9f0b9c8508e0df5
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-13 02:24:43 +0900

    13032022

---
+++ src/programmers/p13032022.java
@@ -0,0 +1,26 @@
+package programmers;
+
+import java.util.*;
+
+//이진 변환 반복하기
+//13032022
+
+public class p13032022 {
+    public static void main(String[] args) throws Exception {
+
+        Solution76 s = new Solution76();
+
+        String a = "110010101001";
+
+       System.out.println(s.solution(a));
+
+    }
+}
+
+class Solution76 {
+
+    public int[] solution(String s) {
+        int[] answer = {};
+        return answer;
+    }
+}
