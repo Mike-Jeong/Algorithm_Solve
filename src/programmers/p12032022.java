79d33d8742932a78392d435e16b211752c3e39d5
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-12 20:20:02 +0900

    12032022

---
+++ src/programmers/p12032022.java
@@ -0,0 +1,26 @@
+package programmers;
+
+import java.util.*;
+
+//전력망을 둘로 나누기
+//12032022
+
+public class p12032022 {
+    public static void main(String[] args) throws Exception {
+
+        Solution75 s = new Solution75();
+
+        int a = 9;
+        int[][] b = { { 1, 3 }, { 2, 3 }, { 3, 4 }, { 4, 5 }, { 4, 6 }, { 4, 7 }, { 7, 8 }, { 7, 9 } };
+
+        System.out.println(s.solution(a, b));
+    }
+}
+
+class Solution75 {
+
+    public int solution(int n, int[][] wires) {
+        int answer = -1;
+        return answer;
+    }
+}
