0462def6c093d9caabaada0dbfbd1ad19af5483e
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-12 20:46:17 +0900

    12032022

--- src/programmers/p12032022.java
+++ src/programmers/p12032022.java
@@ -19,8 +19,38 @@ public class p12032022 {
 
 class Solution75 {
 
+    Map<Integer, List<Integer>> near_nodes = new HashMap<>();
+
     public int solution(int n, int[][] wires) {
         int answer = -1;
+
+        for (int[] is : wires) {
+            if (!near_nodes.containsKey(is[0])) {
+                List<Integer> n_nodes = new LinkedList<>();
+                near_nodes.put(is[0], n_nodes);
+                near_nodes.get(is[0]).add(is[1]);
+            } else {
+                near_nodes.get(is[0]).add(is[1]);
+            }
+
+            if (!near_nodes.containsKey(is[1])) {
+                List<Integer> n_nodes = new LinkedList<>();
+                near_nodes.put(is[1], n_nodes);
+                near_nodes.get(is[1]).add(is[0]);
+            } else {
+                near_nodes.get(is[1]).add(is[0]);
+            }
+        }
+
+        for (int is : near_nodes.keySet()) {
+            if (near_nodes.get(is).size() != 1) {
+
+            }
+        }
         return answer;
+    }
+
+    public void recursion() {
+
     }
 }
