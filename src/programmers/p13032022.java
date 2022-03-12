3c297ba0e7d1a1e8c9695ff11d1411e474a51065
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-13 02:34:51 +0900

    13032022

--- src/programmers/p13032022.java
+++ src/programmers/p13032022.java
@@ -1,6 +1,6 @@
 package programmers;
 
-import java.util.*;
+//import java.util.*;
 
 //이진 변환 반복하기
 //13032022
@@ -20,7 +20,22 @@ public class p13032022 {
 class Solution76 {
 
     public int[] solution(String s) {
-        int[] answer = {};
+
+        int[] answer = new int[2];
+
+        while (s.length() != 1) {
+            int origin_length = s.length();
+            s = s.replaceAll("0", "");
+            int changed_lenght = s.length();
+
+            s = Integer.toBinaryString(changed_lenght);
+
+            answer[0] += 1;
+            answer[1] += (origin_length - changed_lenght);
+        }
+
+        System.out.println(answer[0]);
+        System.out.println(answer[1]);
         return answer;
     }
 }
