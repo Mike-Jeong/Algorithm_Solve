0dc50e5495ea5a071c413b9733307b7b803e3fca
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-13 22:44:08 +0900

    13022022

---
+++ src/programmers/p13032022_02.java
@@ -0,0 +1,27 @@
+package programmers;
+
+import java.util.*;
+
+//[3차] 방금그곡
+//13032022
+
+public class p13032022_02 {
+    public static void main(String[] args) throws Exception {
+
+        Solution77 s = new Solution77();
+
+        String a = "ABCDEFG";
+        String[] b = { "12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF" };
+
+        System.out.println(s.solution(a, b));
+
+    }
+}
+
+class Solution77 {
+
+    public String solution(String m, String[] musicinfos) {
+        String answer = "";
+        return answer;
+    }
+}
