f51f453031742ce0dd856fcfbc0735b5e9972f61
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-15 01:33:42 +0900

    14032022

--- src/programmers/p14032022.java
+++ src/programmers/p14032022.java
@@ -7,9 +7,6 @@ package programmers;
 
 public class p14032022 {
 
-    int enemy_row;
-    int enemy_col;
-
     public static void main(String[] args) throws Exception {
 
         Solution78 s = new Solution78();
@@ -78,3 +75,67 @@ class Solution78 {
         visited[row][col] = false;
     }
 }
+
+
+/*
+class Solution78 {
+
+    boolean[][] visited;
+    int[] order_row = { -1, 0, 1, 0 };
+    int[] order_col = { 0, -1, 0, 1 };
+    int enemy_row;
+    int enemy_col;
+    int answer = -1;
+    int[][] maps;
+
+    public int solution(int[][] maps) {
+
+        this.maps = maps;
+
+        enemy_row = maps.length - 1;
+        enemy_col = maps[0].length - 1;
+
+        visited = new boolean[enemy_row + 1][enemy_col + 1];
+
+        recursion(0, 0, 0);
+
+        return answer;
+    }
+
+    public void recursion(int row, int col, int count) {
+
+        count++;
+        visited[row][col] = true;
+
+        if (row == enemy_row && col == enemy_col) {
+
+            if (answer == -1) {
+                answer = count;
+            } else {
+                answer = Math.min(answer, count);
+            }
+            visited[row][col] = false;
+            return;
+        }
+
+        for (int i = 0; i < 4; i++) {
+
+            try {
+                if (maps[row + order_row[i]][col + order_col[i]] == 1
+                        && !visited[row + order_row[i]][col + order_col[i]]) {
+                    recursion(row + order_row[i], col + order_col[i], count);
+                }
+            } catch (Exception e) {
+                continue;
+            }
+
+        }
+
+        visited[row][col] = false;
+    }
+}
+
+// 시간초과..............
+//DFS 가 아닌 bfs 로 풀어야함
+
+*/
