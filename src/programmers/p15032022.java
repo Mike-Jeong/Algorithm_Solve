02bb661aec850627904f5e7368197c74ed0a0b1f
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-15 05:39:13 +0900

    15032022

--- src/programmers/p15032022.java
+++ src/programmers/p15032022.java
@@ -9,20 +9,74 @@ public class p15032022 {
 
     public static void main(String[] args) throws Exception {
 
-        Solution78 s = new Solution78();
+        Solution79 s = new Solution79();
 
         int a = 3;
-        int[][] b = { {1,1,0}, {1,1,0}, {0,0,1}};
+        int[][] b = { { 1, 1, 0 }, { 1, 1, 1 }, { 0, 1, 1 } };
 
         System.out.println(s.solution(a, b));
 
     }
 }
 
-class Solution78 {
+class Solution79 {
+
+    boolean[] visited;
+    Map<Integer, LinkedList<Integer>> near_nodes = new HashMap<>();
 
     public int solution(int n, int[][] computers) {
+
+        visited = new boolean[n];
         int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (computers[i][j] == 1) {
+                    if (near_nodes.containsKey(i)) {
+                        near_nodes.get(i).add(j);
+                    } else {
+                        LinkedList<Integer> near_n = new LinkedList<>();
+                        near_n.add(j);
+                        near_nodes.put(i, near_n);
+                    }
+                    if (near_nodes.containsKey(j)) {
+                        near_nodes.get(j).add(i);
+                    } else {
+                        LinkedList<Integer> near_n = new LinkedList<>();
+                        near_n.add(i);
+                        near_nodes.put(j, near_n);
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            if (!visited[i]) {
+                recursion(i);
+
+                answer++;
+            }
+        }
+
         return answer;
+    }
+
+    public void recursion(int i){
+        
+        LinkedList<Integer> next_nodes = near_nodes.get(i);
+        visited[i] = true;
+
+        if (next_nodes != null) {
+            int size = next_nodes.size();
+            for (int j = 0; j < size; j++) {
+                int next = next_nodes.poll();
+                if (!visited[next]) {
+                    recursion(next);
+                }
+                
+            }
+        }
+       
     }
 }
