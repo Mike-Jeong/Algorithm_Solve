c1abdfdeeb4bb2be6fc2b44e7811430c3d66aff2
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-15 04:44:34 +0900

    15032022

---
+++ src/programmers/p15032022.java
@@ -0,0 +1,28 @@
+package programmers;
+
+import java.util.*;
+
+//네트워크
+//15032022
+
+public class p15032022 {
+
+    public static void main(String[] args) throws Exception {
+
+        Solution78 s = new Solution78();
+
+        int a = 3;
+        int[][] b = { {1,1,0}, {1,1,0}, {0,0,1}};
+
+        System.out.println(s.solution(a, b));
+
+    }
+}
+
+class Solution78 {
+
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        return answer;
+    }
+}
