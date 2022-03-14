7f172d484775c699a1a0b61c38850479ac689273
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-15 02:29:01 +0900

    14032022

--- src/programmers/p14032022.java
+++ src/programmers/p14032022.java
@@ -1,6 +1,6 @@
 package programmers;
 
-//import java.util.*;
+import java.util.*;
 
 //게임 맵 최단거리
 //14032022
@@ -21,58 +21,44 @@ public class p14032022 {
 
 class Solution78 {
 
-    boolean[][] visited;
-    int[] order_row = { -1, 0, 1, 0 };
-    int[] order_col = { 0, -1, 0, 1 };
-    int enemy_row;
-    int enemy_col;
-    int answer = -1;
-    int[][] maps;
-
     public int solution(int[][] maps) {
 
-        this.maps = maps;
+        int enemy_row = maps.length - 1;
+        int enemy_col = maps[0].length - 1;
 
-        enemy_row = maps.length - 1;
-        enemy_col = maps[0].length - 1;
+        int[][] visited = new int[enemy_row + 1][enemy_col + 1];
+        int[] order_row = { -1, 0, 1, 0 };
+        int[] order_col = { 0, -1, 0, 1 };
 
-        visited = new boolean[enemy_row + 1][enemy_col + 1];
+        Queue<int[]> queue = new LinkedList<>();
 
-        recursion(0, 0, 0);
+        int[] start = {0,0};
+        visited[0][0] = 1;
 
-        return answer;
-    }
+        queue.add(start);
 
-    public void recursion(int row, int col, int count) {
+        while (!queue.isEmpty()) {
 
-        count++;
-        visited[row][col] = true;
+            int[] current = queue.poll();
 
-        if (row == enemy_row && col == enemy_col) {
-
-            if (answer == -1) {
-                answer = count;
-            } else {
-                answer = Math.min(answer, count);
+            if (current[0] == enemy_row && current[1] == enemy_col) {
+                return visited[current[0]][current[1]];
             }
-            visited[row][col] = false;
-            return;
-        }
 
-        for (int i = 0; i < 4; i++) {
-
-            try {
-                if (maps[row + order_row[i]][col + order_col[i]] == 1
-                        && !visited[row + order_row[i]][col + order_col[i]]) {
-                    recursion(row + order_row[i], col + order_col[i], count);
+            for (int i = 0; i < 4; i++) {
+                try {
+                    if (maps[current[0] + order_row[i]][current[1] + order_col[i]] == 1
+                            && visited[current[0] + order_row[i]][current[1] + order_col[i]] == 0) {
+                        queue.add(new int[] {current[0] + order_row[i],current[1] + order_col[i]});
+                        visited[current[0] + order_row[i]][current[1] + order_col[i]] = visited[current[0]][current[1]] + 1;
+                    }
+                } catch (Exception e) {
+                    continue;
                 }
-            } catch (Exception e) {
-                continue;
+    
             }
-
         }
-
-        visited[row][col] = false;
+        return -1;
     }
 }
 
