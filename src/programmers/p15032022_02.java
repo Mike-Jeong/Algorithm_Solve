0e53c5a83976b3b6ca55a2387e8640efb87eb136
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-15 16:59:25 +0900

    15032022

---
+++ src/programmers/p15032022_02.java
@@ -0,0 +1,27 @@
+package programmers;
+
+import java.util.*;
+
+//쿼드압축 후 개수 세기
+//15032022
+
+public class p15032022_02 {
+
+    public static void main(String[] args) throws Exception {
+
+        Solution79 s = new Solution79();
+
+        int[][] a = { { 1, 1, 0, 0 }, { 1, 0, 0, 0 }, { 1, 0, 0, 1 }, { 1, 1, 1, 1 } };
+
+        System.out.println(s.solution(a));
+
+    }
+}
+
+class Solution79 {
+
+    public int[] solution(int[][] arr) {
+        int[] answer = {};
+        return answer;
+    }
+}
