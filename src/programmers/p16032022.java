e4eb2f4a0a8b489d668822f732716b5e97088605
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-16 00:25:42 +0900

    16032022

---
+++ src/programmers/p16032022.java
@@ -0,0 +1,28 @@
+package programmers;
+
+import java.util.*;
+
+//베스트앨범
+//16032022
+
+public class p16032022 {
+
+    public static void main(String[] args) throws Exception {
+
+        Solution81 s = new Solution81();
+
+        String[] a = {"classic", "pop", "classic", "classic", "pop"};
+        int[] b = {500, 600, 150, 800, 2500};
+
+        System.out.println(s.solution(a, b));
+
+    }
+}
+
+class Solution81 {
+
+    public int[] solution(String[] genres, int[] plays) {
+        int[] answer = {};
+        return answer;
+    }
+}
