77efd9c44631257ace49b1c0c67fd2690fc11466
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-17 23:04:29 +0900

    17032022

---
+++ src/programmers/p17032022.java
@@ -0,0 +1,27 @@
+package programmers;
+
+import java.util.*;
+
+//방문 길이
+//17032022
+
+public class p17032022 {
+
+    public static void main(String[] args) throws Exception {
+
+        Solution82 s = new Solution82();
+
+        String a = "ULURRDLLU";
+
+        System.out.println(s.solution(a));
+
+    }
+}
+
+class Solution82 {
+
+    public int solution(String dirs) {
+        int answer = 0;
+        return answer;
+    }
+}
