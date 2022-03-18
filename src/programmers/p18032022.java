7f9ca6c9a102da9a2717dc88594683f30b91f190
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-18 23:25:24 +0900

    18032022

---
+++ src/programmers/p18032022.java
@@ -0,0 +1,27 @@
+package programmers;
+
+import java.util.*;
+
+//여행경로
+//18032022
+
+public class p18032022 {
+
+    public static void main(String[] args) throws Exception {
+
+        Solution83 s = new Solution83();
+
+        String[][] a = { { "ICN", "JFK" }, { "HND", "IAD" }, { "JFK", "HND" } };
+
+        System.out.println(s.solution(a));
+
+    }
+}
+
+class Solution83 {
+
+    public String[] solution(String[][] tickets) {
+        String[] answer = {};
+        return answer;
+    }
+}
