2c353dc1df011036075b97b013c3899db03eda4f
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-20 05:38:14 +0900

    20032022

---
+++ src/programmers/p20032022.java
@@ -0,0 +1,28 @@
+package programmers;
+
+import java.util.*;
+
+//순위
+//20032022
+
+public class p20032022 {
+
+    public static void main(String[] args) throws Exception {
+
+        Solution85 s = new Solution85();
+
+        int a = 5;
+        int[][] b = { { 4, 3 }, { 4, 2 }, { 3, 2 }, { 1, 2 }, { 2, 5 } };
+
+        System.out.println(s.solution(a, b));
+
+    }
+}
+
+class Solution85 {
+
+    public int solution(int n, int[][] results) {
+        int answer = 0;
+        return answer;
+    }
+}
