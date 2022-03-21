cd63bdd86962ef7235b03fe23a9769cd25b9fbd6
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-21 21:38:43 +0900

    21032022

---
+++ src/programmers/p21032022.java
@@ -0,0 +1,27 @@
+package programmers;
+
+import java.util.*;
+
+//다음 큰 숫자
+//21032022
+
+public class p21032022 {
+
+    public static void main(String[] args) throws Exception {
+
+        Solution86 s = new Solution86();
+
+        int a = 3;
+
+        System.out.println(s.solution(a));
+
+    }
+}
+
+class Solution86 {
+
+    public int solution(int n) {
+        int answer = 0;
+        return answer;
+    }
+}
