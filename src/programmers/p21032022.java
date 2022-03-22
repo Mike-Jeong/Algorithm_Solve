7a52ca75dd5cb55549c7a1b7c3af9a3b3154920b
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-22 23:10:00 +0900

    22032022

--- src/programmers/p21032022.java
+++ src/programmers/p21032022.java
@@ -1,6 +1,6 @@
 package programmers;
 
-import java.util.*;
+//import java.util.*;
 
 //다음 큰 숫자
 //21032022
---
+++ src/programmers/p22032022.java
@@ -0,0 +1,28 @@
+package programmers;
+
+import java.util.*;
+
+//스킬트리
+//22032022
+
+public class p22032022 {
+
+    public static void main(String[] args) throws Exception {
+
+        Solution87 s = new Solution87();
+
+        String a = "CBD";
+        String[] b = { "BACDE", "CBADF", "AECB", "BDA" };
+
+        System.out.println(s.solution(a, b));
+
+    }
+}
+
+class Solution87 {
+
+    public int solution(String skill, String[] skill_trees) {
+        int answer = -1;
+        return answer;
+    }
+}
