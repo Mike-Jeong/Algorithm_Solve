ff1134b445bd170ba3ba56d2b07a185972785f1a
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-23 23:15:14 +0900

    23032022

---
+++ src/programmers/p23032022.java
@@ -0,0 +1,27 @@
+package programmers;
+
+import java.util.*;
+
+//하샤드 수
+//23032022
+
+public class p23032022 {
+
+    public static void main(String[] args) throws Exception {
+
+        Solution88 s = new Solution88();
+
+        int a = 10;
+
+        System.out.println(s.solution(a));
+
+    }
+}
+
+class Solution88 {
+
+    public boolean solution(int x) {
+        boolean answer = true;
+        return answer;
+    }
+}
