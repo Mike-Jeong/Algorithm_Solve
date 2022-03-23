40a53a78b5d7101a1d5717456fffb6181bfddbae
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-24 05:00:30 +0900

    24032022

---
+++ src/programmers/p24032022.java
@@ -0,0 +1,28 @@
+package programmers;
+
+import java.util.*;
+
+//피로도
+//24032022
+
+public class p24032022 {
+
+    public static void main(String[] args) throws Exception {
+
+        Solution89 s = new Solution89();
+
+        int a = 80;
+        int[][] b = {{80,20},{50,40},{30,10}};
+
+        System.out.println(s.solution(a, b));
+
+    }
+}
+
+class Solution89 {
+
+    public int solution(int k, int[][] dungeons) {
+        int answer = -1;
+        return answer;
+    }
+}
