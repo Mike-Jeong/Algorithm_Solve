452dd78900d235aedc7683e81e4d6310fa709d1c
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-24 00:19:46 +0900

    23022022

--- src/programmers/p23032022.java
+++ src/programmers/p23032022.java
@@ -1,6 +1,6 @@
 package programmers;
 
-import java.util.*;
+//import java.util.*;
 
 //하샤드 수
 //23032022
@@ -11,7 +11,7 @@ public class p23032022 {
 
         Solution88 s = new Solution88();
 
-        int a = 10;
+        int a = 11;
 
         System.out.println(s.solution(a));
 
@@ -21,7 +21,22 @@ public class p23032022 {
 class Solution88 {
 
     public boolean solution(int x) {
-        boolean answer = true;
-        return answer;
+
+        int num = x;
+        int sum = 0;
+
+        while (num != 0) {
+
+            sum += (num % 10);
+            num = num / 10;
+        }
+
+        sum += num;
+
+        if ((x % sum) == 0) {
+            return true;
+        }
+
+        return false;
     }
 }
