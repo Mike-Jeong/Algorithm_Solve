f35bcb5ee20a8f7d7f5381ad4607328b18af6e98
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-24 05:10:49 +0900

    24032022

--- src/programmers/p24032022.java
+++ src/programmers/p24032022.java
@@ -12,7 +12,7 @@ public class p24032022 {
         Solution89 s = new Solution89();
 
         int a = 80;
-        int[][] b = {{80,20},{50,40},{30,10}};
+        int[][] b = { { 80, 20 }, { 50, 40 }, { 30, 10 } };
 
         System.out.println(s.solution(a, b));
 
@@ -21,8 +21,32 @@ public class p24032022 {
 
 class Solution89 {
 
+    LinkedList<int[]> list = new LinkedList<>();
+    int c = 0;
+
     public int solution(int k, int[][] dungeons) {
-        int answer = -1;
-        return answer;
+
+        for (int[] is : dungeons) {
+            list.add(is);
+        }
+
+        recursion(k, 0);
+
+        return c;
+    }
+
+    public void recursion(int k, int count) {
+
+        if (list.size() != 0) {
+            for (int i = 0; i < list.size(); i++) {
+                int[] now = list.poll(); 
+                if (now[0] <= k) {
+                    recursion(k-now[1], count + 1);
+                }
+                list.add(now);
+            }
+        }
+
+        c = Math.max(c, count);
     }
 }
