42fc566074eb2f7ce3b050def02946e8b3a8c577
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-25 03:57:17 +0900

    25032022

---
+++ src/programmers/p25032022.java
@@ -0,0 +1,31 @@
+package programmers;
+
+import java.util.*;
+
+//피로도
+//25032022
+
+public class p25032022 {
+
+    public static void main(String[] args) throws Exception {
+
+        Solution90 s = new Solution90();
+
+        int[][] a = { { 0, 1, 1, 1 }, { 1, 1, 1, 1 }, { 1, 1, 1, 1 }, { 0, 0, 1, 0 } };
+
+        System.out.println(s.solution(a));
+
+    }
+}
+
+class Solution90 {
+
+    public int solution(int[][] board) {
+        int answer = 1234;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
