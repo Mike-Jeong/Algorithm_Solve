af43e301ff8fec8d6d0b1457d9a9fe5f28440058
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-25 06:38:40 +0900

    25032022

--- src/programmers/p25032022.java
+++ src/programmers/p25032022.java
@@ -2,7 +2,7 @@ package programmers;
 
 import java.util.*;
 
-//피로도
+//가장 큰 정사각형 찾기
 //25032022
 
 public class p25032022 {
@@ -11,8 +11,7 @@ public class p25032022 {
 
         Solution90 s = new Solution90();
 
-        int[][] a = { { 0, 1, 1, 1 }, { 1, 1, 1, 1 }, { 1, 1, 1, 1 }, { 0, 0, 1, 0 } };
-
+        String a = "3people unFollowed me";
         System.out.println(s.solution(a));
 
     }
@@ -20,62 +19,19 @@ public class p25032022 {
 
 class Solution90 {
 
-    boolean[][] visited;
-    Queue<int[]> queue = new LinkedList<>();
-    int[][] board;
-    int[] row_order = { 0, 1, 1 };
-    int[] col_order = { 1, 1, 0 };
-    int ans = 0;
+    public String solution(String s) {
+        String ans = "";
 
-    public int solution(int[][] board) {
-
-        this.board = board;
-        visited = new boolean[board.length][board[0].length];
-
-        for (int i = 0; i < board.length - 1; i++) {
-            for (int j = 0; j < board[0].length - 1; j++) {
-                if (board[i][j] == 1 && !visited[i][j]) {
-                    queue.add(new int[] { i, j });
-                    int count = 0;
-
-                    while (!queue.isEmpty()) {
-
-                        int[] now = queue.poll();
-                        if (!visited[now[0]][now[1]]) {
-                            count++;
-                            bfs(now[0], now[1]);
-                        }
-
-                    }
-
-                    ans = Math.max(count, ans);
-                }
-            }
+        String[] srr = s.toLowerCase().split("");
+        
+        boolean isFirst = true;
+        
+        for(int i = 0; i < srr.length; i++) {
+            
+            ans += isFirst ? srr[i].toUpperCase() : srr[i];
+            isFirst = srr[i].equals(" ") ? true : false;            
         }
+        
         return ans;
-    }
-
-    public void bfs(int r, int c) {
-
-        visited[r][c] = true;
-
-        LinkedList<int[]> ck = new LinkedList<>();
-
-        for (int i = 0; i < 3; i++) {
-            try {
-                if (!visited[r + row_order[i]][c + col_order[i]]) {
-                    if (board[r + row_order[i]][c + col_order[i]] != 1) {
-                        return;
-                    }
-                    ck.add(new int[] { r + row_order[i], c + col_order[i] });
-                }
-
-            } catch (Exception e) {
-                return;
-            }
-        }
-
-        queue.addAll(ck);
-
     }
 }
