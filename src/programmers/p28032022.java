291b0ebf63bc473006b87661296d7676e9be9160
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-28 18:03:19 +0900

    28032022

---
+++ src/programmers/p28032022.java
@@ -0,0 +1,27 @@
+package programmers;
+
+//import java.util.*;
+
+//정수 내림차순으로 배치하기
+//28032022
+
+public class p28032022 {
+
+    public static void main(String[] args) throws Exception {
+
+        Solution92 s = new Solution92();
+
+        long a = 437674;
+
+        System.out.println(s.solution(a));
+
+    }
+}
+
+class Solution92 {
+
+    public long solution(long n) {
+        long answer = 0;
+        return answer;
+    }
+}
