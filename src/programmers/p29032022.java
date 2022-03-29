f35484998db1aaf1be272fa27e1481d2a04c7b33
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-29 18:40:19 +0900

    29032022

---
+++ src/programmers/p29032022.java
@@ -0,0 +1,28 @@
+package programmers;
+
+import java.util.*;
+
+//양궁대회
+//29032022
+
+public class p29032022 {
+
+    public static void main(String[] args) throws Exception {
+
+        Solution93 s = new Solution93();
+
+        int a = 5;
+        int[] b = {2,1,1,1,0,0,0,0,0,0,0};
+
+        System.out.println(s.solution(a, b));
+
+    }
+}
+
+class Solution93 {
+
+    public int[] solution(int n, int[] info) {
+        int[] answer = {};
+        return answer;
+    }
+}
