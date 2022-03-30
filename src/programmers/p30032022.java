5a165480d7745544fb98fe2b8b501dba2cb057c0
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-30 21:04:19 +0900

    30032022

---
+++ src/programmers/p30032022.java
@@ -0,0 +1,27 @@
+package programmers;
+
+import java.util.*;
+
+//N개의 최소공배수
+//30032022
+
+public class p30032022 {
+
+    public static void main(String[] args) throws Exception {
+
+        Solution94 s = new Solution94();
+
+        int[] a = { 2, 6, 8, 14 };
+
+        System.out.println(s.solution(a));
+
+    }
+}
+
+class Solution94 {
+
+    public int solution(int[] arr) {
+        int answer = 0;
+        return answer;
+    }
+}
