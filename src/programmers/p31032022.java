7f2cc4debb7db726041d4c06147bdf01f3bb8fa8
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-03-31 22:17:04 +0900

    31032022

--- src/programmers/p31032022.java
+++ src/programmers/p31032022.java
@@ -23,9 +23,11 @@ class Solution95 {
     public int solution(int n) {
         int answer = 0;
 
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
+        while (n != 0) {
+            answer += (n % 10);
+            n /= 10;
+        }
+        
         return answer;
     }
 }
