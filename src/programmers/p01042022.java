da610bfb8d297db05f810f1736b572de19cf2386
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-04-01 21:56:02 +0900

    01042022

--- src/programmers/p01042022.java
+++ src/programmers/p01042022.java
@@ -22,7 +22,48 @@ public class p01042022 {
 class Solution96 {
 
     public int solution(int[][] board, int[][] skill) {
+
         int answer = 0;
+
+        int[][] board_c = new int[board.length + 1][board[0].length + 1];
+
+        for (int[] is : skill) {
+            if (is[0] == 1) {
+                board_c[is[1]][is[2]] += - is[5];
+                board_c[is[3] + 1][is[4] + 1] += - is[5];
+                board_c[is[1]][is[4] + 1] += is[5];
+                board_c[is[3] + 1][is[2]] += is[5];
+            } else {
+                board_c[is[1]][is[2]] += is[5];
+                board_c[is[3] + 1][is[4] + 1] += is[5];
+                board_c[is[1]][is[4] + 1] += - is[5];
+                board_c[is[3] + 1][is[2]] += - is[5];
+            }
+           
+        }
+
+        for (int i = 0; i < board_c[0].length; i++) {
+            for (int j = 1; j < board_c.length; j++) {
+                board_c[j][i] += board_c[j - 1][i];
+            }
+        }
+
+        for (int i = 0; i < board_c.length; i++) {
+            for (int j = 1; j < board_c[0].length; j++) {
+                board_c[i][j] += board_c[i][j - 1];
+            }
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                board[i][j] += board_c[i][j];
+
+                if (board[i][j] > 0) {
+                    answer++;
+                }
+            }
+        }
+
         return answer;
     }
 }
