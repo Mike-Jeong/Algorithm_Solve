98b64a1c7b49be83abd0c2d5aaa8656665a0c72e
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-04-02 19:47:28 +0900

    02042022

---
+++ src/programmers/p02042022.java
@@ -0,0 +1,27 @@
+package programmers;
+
+//import java.util.*;
+
+//2개 이하로 다른 비트
+//02042022
+
+public class p02042022 {
+
+    public static void main(String[] args) throws Exception {
+
+        Solution97 s = new Solution97();
+
+        long[] a = { 2, 7 };
+
+        System.out.println(s.solution(a));
+
+    }
+}
+
+class Solution97 {
+
+    public long[] solution(long[] numbers) {
+        long[] answer = {};
+        return answer;
+    }
+}
