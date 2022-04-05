fe945ddedb938153b75ec1f0d701784aac65ca02
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-04-06 02:38:16 +0900

    05042022

---
+++ src/programmers/p05042022.java
@@ -0,0 +1,29 @@
+package programmers;
+
+//import java.util.*;
+
+//야근 지수
+//05042022
+
+public class p05042022 {
+
+    public static void main(String[] args) throws Exception {
+
+        Solution98 s = new Solution98();
+
+        int a = 4;
+
+        int[] b = { 4, 3, 3 };
+
+        System.out.println(s.solution(a, b));
+
+    }
+}
+
+class Solution98 {
+
+    public long solution(int n, int[] works) {
+        long answer = 0;
+        return answer;
+    }
+}
