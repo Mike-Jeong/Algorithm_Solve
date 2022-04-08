14bd3bc270eca32b779f07d903c00e1a722adbeb
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-04-09 01:22:20 +0900

    08042022

--- src/backjoon/b08042022.java
+++ src/backjoon/b08042022.java
@@ -1,11 +1,43 @@
 package backjoon;
 
-import java.io.*;
 import java.util.*;
 
 public class b08042022 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-       
+        Scanner sc = new Scanner(System.in);
+
+        int N = 1000000;
+
+        boolean check[] = new boolean[N + 1];
+        for (int i = 2; i <= N; i++) {
+            check[i] = true;
+        }
+
+        // 에라토스테네스의 체 //소수찾기
+        for (int i = 2; i <= Math.sqrt(N); i++) {
+            for (int j = i + i; j <= N; j += i) {
+                check[j] = false;
+            }
+        }
+
+        while (true) {
+            int n = sc.nextInt();
+
+            if (n == 0) {
+                break;
+            }
+
+            int tmp = n / 2;
+
+            for (int j = 2; j <= tmp; j++) {
+                if (check[j] && check[n - j]) {
+                    System.out.println(n + " = " + j + " + " + (n - j));
+                    break;
+                }
+            }
+        }
+
+        sc.close();
     }
 }
