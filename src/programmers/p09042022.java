4c1e3fcbd1b475370314c63db8e40a3d0f0d676c
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-04-10 02:51:27 +0900

    09042022

--- src/programmers/p09042022.java
+++ src/programmers/p09042022.java
@@ -1,28 +1,55 @@
 package programmers;
 
-//import java.util.*;
+import java.util.*;
 
-//사라지는 발판
+//이중우선순위큐
 //09042022
 
 public class p09042022 {
 
     public static void main(String[] args) throws Exception {
 
-        Solution99 s = new Solution99();
+        Solution100 s = new Solution100();
 
-        int[][] a = {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
-        int[] b = {1,0};
-        int[] c = {1,2};
-        System.out.println(s.solution(a,b,c));
+        String[] a = { "I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333" };
+
+        System.out.println(s.solution(a));
 
     }
 }
 
-class Solution99 {
+class Solution100 {
 
-    public int solution(int[][] board, int[] aloc, int[] bloc) {
-        int answer = -1;
-        return answer;
+    public int[] solution(String[] operations) {
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        PriorityQueue<Integer> priorityQueue_r = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (String orders : operations) {
+            String[] order = orders.split(" ");
+            String tag = order[0];
+
+            if (tag.equals("I")) {
+                priorityQueue.add(Integer.parseInt(order[1]));
+                priorityQueue_r.add(Integer.parseInt(order[1]));
+            } else {
+                if (!priorityQueue.isEmpty()) {
+                    if (order[1].equals("1")) {
+                        int target = priorityQueue_r.poll();
+                        priorityQueue.remove(target);
+                    } else {
+                        int target = priorityQueue.poll();
+                        priorityQueue_r.remove(target);
+                    }
+                }
+            }
+        }
+
+        if (priorityQueue.isEmpty()) {
+            return new int[] { 0, 0 };
+        } else {
+            return new int[] { priorityQueue_r.poll(), priorityQueue.poll() };
+        }
+
     }
 }
