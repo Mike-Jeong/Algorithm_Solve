31ababe1ddbc16c1e40b281bb1eced10eb63b41d
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-04-12 22:35:24 +0900

    12042022

---
+++ src/backjoon/b12042022.java
@@ -0,0 +1,30 @@
+package backjoon;
+
+import java.util.*;
+
+//터렛
+//12042022
+
+public class b12042022 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int count = sc.nextInt();
+
+        while (count > 0) {
+
+            int x1 = sc.nextInt();
+            int y1 = sc.nextInt();
+            int r1 = sc.nextInt();
+
+            int x2 = sc.nextInt();
+            int y2 = sc.nextInt();
+            int r2 = sc.nextInt();
+
+            count--;
+        }
+
+        sc.close();
+    }
+}
