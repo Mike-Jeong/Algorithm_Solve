3f242548d2a20a26336284113806c41a158391bb
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-04-12 23:04:39 +0900

    12042022

--- src/backjoon/b12042022.java
+++ src/backjoon/b12042022.java
@@ -22,9 +22,40 @@ public class b12042022 {
             int y2 = sc.nextInt();
             int r2 = sc.nextInt();
 
+            find_coordinateNum(x1, y1, x2, y2, r1, r2);
+
             count--;
         }
 
         sc.close();
+    }
+
+    public static void find_coordinateNum(int x1, int y1, int x2, int y2, int r1, int r2) {
+
+        int distance_2 = (int)(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+ 
+		if(x1 == x2 && y1 == y2 && r1 == r2) {
+            System.out.println(-1);
+		}
+
+		else if(distance_2 > Math.pow(r1 + r2, 2)) {
+            System.out.println(0);
+		}
+ 
+		else if(distance_2 < Math.pow(r2 - r1, 2)) {
+            System.out.println(0);
+		}
+		
+		else if(distance_2 == Math.pow(r2 - r1, 2)) {
+            System.out.println(1);
+		}
+        
+		else if(distance_2 == Math.pow(r1 + r2, 2)) {
+            System.out.println(1);
+		}
+		
+		else {
+            System.out.println(2);
+		}
     }
 }
