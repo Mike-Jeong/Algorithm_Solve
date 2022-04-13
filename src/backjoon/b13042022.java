45f8417f6ab72d8c05c7ee37ec2547ecbfcc1cd4
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-04-13 23:11:04 +0900

    13042022

---
+++ src/backjoon/b13042022.java
@@ -0,0 +1,27 @@
+package backjoon;
+
+import java.util.*;
+
+//피보나치 함수
+//13042022
+
+public class b13042022 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int count = sc.nextInt();
+
+        while (count > 0) {
+
+            int num = sc.nextInt();
+           
+
+            count--;
+        }
+
+        sc.close();
+    }
+
+   
+}
