d000001c147a7f71e9acd42fbdd5180cd2904625
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-04-21 23:16:09 +0900

    21042022

--- src/backjoon/b21042022.java
+++ src/backjoon/b21042022.java
@@ -13,7 +13,22 @@ public class b21042022 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
 		
+		Scanner in = new Scanner(System.in);
+		
+		int T = in.nextInt();
+		
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		
+		while(T-- > 0) {
+			int N = in.nextInt();
+			max = N > max ? N : max;
+			min = N < min ? N : min;
+            
+		}
+		System.out.println(max * min);
 
+		in.close();
 	}
 
 }
