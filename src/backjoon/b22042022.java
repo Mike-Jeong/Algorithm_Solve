55ecf118fe982c7f41e192f0e4a28754b23cce94
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-04-22 23:22:48 +0900

    22042022

--- src/backjoon/b22042022.java
+++ src/backjoon/b22042022.java
@@ -8,11 +8,30 @@ import java.util.*;
 
 public class b22042022 {
 
-
-
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
-		
+		Scanner sc = new Scanner(System.in);
+ 
+        int n = sc.nextInt();
+ 
+        PriorityQueue<Integer> a = new PriorityQueue<>();
+		PriorityQueue<Integer> b = new PriorityQueue<>(Collections.reverseOrder());
+ 
+        for (int i = 0; i < n; i++) {
+            a.add(sc.nextInt());
+        }
+        for (int i = 0; i < n; i++) {
+            b.add(sc.nextInt());
+        }
+
+		int total = 0;
+		for (int i = 0; i < n; i++) {
+			total += (a.poll() * b.poll());
+		}
+
+		System.out.println(total);
+
+		sc.close();
 		
 	}
 
