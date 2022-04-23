85cb21074153407faaa641da52e24b6f765c0979
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-04-23 23:09:58 +0900

    23042022

---
+++ src/backjoon/b23042022.java
@@ -0,0 +1,17 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//주사위
+//23042022
+
+public class b23042022 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		
+		
+	}
+
+}
