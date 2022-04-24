a241c746aafc9ca1286d4b72d0a8006f0a2061c8
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-04-24 21:58:22 +0900

    24042022

---
+++ src/backjoon/b24042022.java
@@ -0,0 +1,17 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//램프
+//24042022
+
+public class b24042022 {
+
+
+	public static void main(String[] args) throws Exception {
+
+		
+
+	}
+}
