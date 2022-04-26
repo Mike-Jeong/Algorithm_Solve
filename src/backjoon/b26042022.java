3ca414952ad8609657c78d3873760955bafb559d
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-04-26 21:36:26 +0900

    26042022

--- src/backjoon/b26042022.java
+++ src/backjoon/b26042022.java
@@ -7,7 +7,40 @@ import java.util.*;
 //26042022
 
 public class b26042022 {
-	public static void main(String args[]) throws IOException {
-		
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		int N = stoi(st.nextToken());
+		int arr[] = new int[N];
+		st = new StringTokenizer(in.readLine());
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = stoi(st.nextToken());
+		}
+		st = new StringTokenizer(in.readLine());
+		int M = stoi(st.nextToken());
+		Arrays.sort(arr);
+		int left = 0, right = 987654321;
+		boolean find = false;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > M) {
+				right = Math.min(right, arr[i]);
+			} else if (arr[i] < M) {
+				left = Math.max(left, arr[i]);
+			} else {
+				find = true;
+				break;
+			}
+		}
+		int L = (M - left - 1);
+		int R = (right - M - 1);
+		int answer = L + R + (L * R);
+		if (find)
+			System.out.println(0);
+		else
+			System.out.println(answer);
+	}
+
+	static int stoi(String s) {
+		return Integer.valueOf(s);
 	}
 }
