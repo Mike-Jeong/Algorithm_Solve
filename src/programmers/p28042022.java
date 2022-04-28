3b9df7077b4cc76fcfd79b4d021b152044537056
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-04-28 22:04:12 +0900

    28042022

---
+++ src/programmers/p28042022.java
@@ -0,0 +1,26 @@
+package programmers;
+
+import java.util.*;
+
+public class p28042022 {
+
+    public static void main(String[] args) throws Exception {
+
+        Solution105 s = new Solution105();
+
+        int a = 2;
+
+        int b = 5;
+
+        System.out.println(s.solution(a, b));
+
+    }
+}
+
+class Solution105 {
+
+    public int solution(int x1, int y1, int x2, int y2) {
+        int answer = 0;
+        return answer;
+    }
+}
