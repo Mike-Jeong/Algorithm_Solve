1c4749eb615b59216ecb782f19594774d807f574
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-04-30 23:13:47 +0900

    30042022

--- src/backjoon/b30042022.java
+++ src/backjoon/b30042022.java
@@ -9,9 +9,33 @@ import java.io.*;
 public class b30042022 {
 	public static void main(String[] args) throws IOException {
 
-		
-		
+		boolean[] nums = new boolean[10001];
+
+		for (int i = 1; i <= 10000; i++) {
+
+			int n = i;
+
+			while (n <= 10000) {
+
+				int cur = 0;
+				cur += n;
+				while (n > 0) {
+					cur += (n % 10);
+					n /= 10;
+				}
+
+				if (cur <= 10000) {
+					nums[cur] = true;
+				}
+
+				n = cur;
+			}
+
+			if (!nums[i]) {
+				System.out.println(i);
+			}
+
+		}
 
 	}
 }
-
