03855cdd05c8f017201922226adda2e19b8376e3
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-05-02 22:33:13 +0900

    02052022

--- src/backjoon/b02052022.java
+++ src/backjoon/b02052022.java
@@ -8,9 +8,43 @@ import java.util.*;
 
 public class b02052022 {
 
+	static int count = Integer.MAX_VALUE;
+
 	public static void main(String[] args) throws IOException {
 
-	
+		Scanner in = new Scanner(System.in);
+
+		int num = in.nextInt();
+
+		recursion(num, 0);
+
+		System.out.println(count);
+
+		in.close();
 
 	}
+
+	static void recursion(int num, int c) {
+
+		if (num == 1) {
+
+			if (c < count) {
+				count = c;
+			}
+
+			return;
+		}
+
+		if (num % 3 == 0) {
+			recursion(num/3, c + 1);
+		}
+
+		if (num % 2 == 0) {
+			recursion(num/2, c + 1);
+		}
+
+		recursion(num - 1, c + 1);
+	}
 }
+
+//dp로 해결해야함... 다시 풀것
