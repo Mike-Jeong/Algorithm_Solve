73ec80be051efb3fc3d9630f16ca93acd76f7c43
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-05-04 23:16:52 +0900

    04052022

--- src/backjoon/b04052022.java
+++ src/backjoon/b04052022.java
@@ -7,13 +7,43 @@ import java.util.*;
 //04052022
 
 public class b04052022 {
+
+    static int[] nums = { 1, 2, 3 };
+    static int count;
+    static int target;
+
     public static void main(String[] args) throws IOException {
 
         Scanner in = new Scanner(System.in);
 
-		int num = in.nextInt();
+        int N = in.nextInt();
 
-		
+        for (int i = 0; i < N; i++) {
+            count = 0;
+            target = in.nextInt();
+            recursion(0);
+            System.out.println(count);
+        }
+
+        in.close();
+
+    }
+
+    public static void recursion(int sum) {
+        
+        if (sum >= target) {
+            if (sum == target) {
+                count++;
+            }
+
+            return;
+        }
+
+        for (int i = 0; i < 3; i++) {
+            sum += nums[i];
+            recursion(sum);
+            sum -= nums[i];
+        }
     }
 
 }
