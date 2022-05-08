ddbaa616ca4d65332d589fed932d2f41300cb331
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-05-08 23:41:54 +0900

    08052022

--- src/backjoon/b08052022.java
+++ src/backjoon/b08052022.java
@@ -10,7 +10,30 @@ public class b08052022 {
 
     public static void main(String[] args) throws IOException {
 
-        
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        if (N == 1) {
+            System.out.println(1);
+        } else {
+
+            int[] nums = new int[N + 1];
+
+            nums[0] = 0;
+            nums[1] = 1;
+            nums[2] = 2;
+    
+            for (int i = 3; i < nums.length; i++) {
+                nums[i] = (nums[i - 1] + nums[i - 2]) % 10007;
+            }
+    
+            System.out.println(nums[N]);
+    
+
+        }
+
+        sc.close();
 
     }
 
