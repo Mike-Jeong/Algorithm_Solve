6b14d0824bf6903c6e5c50748f9d35f13daa3744
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-05-10 22:24:41 +0900

    10052022

---
+++ src/backjoon/b10052022.java
@@ -0,0 +1,18 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//N과 M(1) (15649)
+//10052022
+
+public class b10052022 {
+
+	
+	public static void main(String[] args) {
+
+
+		
+	}
+	
+}
