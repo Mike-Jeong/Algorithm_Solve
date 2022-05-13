2e993def140f01be5d20317c8ba7e6999cadaa69
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-05-13 22:51:17 +0900

    13052022

--- src/backjoon/b13052022.java
+++ src/backjoon/b13052022.java
@@ -10,14 +10,38 @@ public class b13052022 {
 
 	public static void main(String[] args) throws IOException {
 
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		Stack<Integer> stack = new Stack<>();
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int start = 0;
+		
+		while(N -- > 0) {
+			
+			int value = Integer.parseInt(br.readLine());
+			
+			if(value > start) {
 
-        int N = sc.nextInt();
-
-       
-
-        sc.close();
-
-    }
-
+				for(int i = start + 1; i <= value; i++) {
+					stack.push(i);
+					sb.append('+').append('\n');	
+				}
+				start = value; 	
+			}
+			
+			else if(stack.peek() != value) {
+				System.out.println("NO");
+				return;		
+			}
+			
+			stack.pop();
+			sb.append('-').append('\n');
+			
+		}
+		
+		System.out.println(sb);
+	}
 }
