490115265c9de0aeace03ed64369311f8eafa1fd
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-05-13 22:38:46 +0900

    13052022

---
+++ src/backjoon/b13052022.java
@@ -0,0 +1,23 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//스택 수열
+//13052022
+
+public class b13052022 {
+
+	public static void main(String[] args) throws IOException {
+
+		Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+       
+
+        sc.close();
+
+    }
+
+}
