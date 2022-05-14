78106d3001ec628fb5c1699f2e3874cc6a1093a0
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-05-14 22:29:37 +0900

    14052022

--- src/backjoon/b14052022.java
+++ src/backjoon/b14052022.java
@@ -1,7 +1,7 @@
 package backjoon;
 
 import java.io.*;
-import java.util.*;
+//import java.util.*;
 
 //이친수
 //14052022
@@ -10,6 +10,15 @@ public class b14052022 {
 
 	public static void main(String[] args) throws IOException {
 
-		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int count = Integer.parseInt(br.readLine());
+		long d[] = new long[count + 1];
+
+		d[0] = 0;
+		d[1] = 1;
+		for (int i = 2; i <= count; i++) {
+			d[i] = d[i - 1] + d[i - 2];
+		}
+		System.out.println(d[count]);
 	}
 }
