e4f0b194a8a6ec00774da357dd1873f9a673a708
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-05-15 23:13:02 +0900

    15052022

---
+++ src/backjoon/b15052022.java
@@ -0,0 +1,16 @@
+package backjoon;
+
+import java.io.*;
+//import java.util.*;
+
+//N과 M (2)
+//15052022
+
+public class b15052022 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+}
