ae7fa1717332c7bfff5203b88ab74e0e59ba5144
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-05-16 22:01:02 +0900

    16052022

---
+++ src/backjoon/b16052022.java
@@ -0,0 +1,27 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//퇴사
+//16052022
+
+public class b16052022 {
+
+	static int range;
+	static int target;
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+	}
+
+	public static void recursion() {
+
+		
+		
+	}
+}
