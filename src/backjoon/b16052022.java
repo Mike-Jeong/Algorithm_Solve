dc643d4fa28cbd9f3191f1849d547043f3e22179
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-05-16 22:22:54 +0900

    16052022

--- src/backjoon/b16052022.java
+++ src/backjoon/b16052022.java
@@ -8,20 +8,47 @@ import java.util.*;
 
 public class b16052022 {
 
-	static int range;
-	static int target;
+	static int N;
+	static int[] days;
+	static int[] profit;
+	static int maxProfit = 0;
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
+		N = Integer.valueOf(st.nextToken());
+		days = new int[N];
+		profit = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			days[i] = Integer.parseInt(st.nextToken());
+			profit[i] = Integer.parseInt(st.nextToken());
+
+		}
+
+		recursion(0, 0);
+
+		System.out.println(maxProfit);
+
 
 	}
 
-	public static void recursion() {
+	public static void recursion(int index, int sum) {
 
-		
+		if (index >= N) {
+			if (index == N) {
+				maxProfit = Math.max(maxProfit, sum);
+			}
+
+			return;
+		}
+
+		recursion(index + 1, sum);
+		recursion(index + days[index], sum + profit[index]);
 		
 	}
 }
