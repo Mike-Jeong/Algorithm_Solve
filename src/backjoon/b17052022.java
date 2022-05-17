d52eece44871b50501c1c73046cdeb874932f9dc
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-05-17 23:16:09 +0900

    17052022

--- src/backjoon/b17052022.java
+++ src/backjoon/b17052022.java
@@ -13,8 +13,46 @@ public class b17052022 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
+		int N = Integer.valueOf(st.nextToken());
 
+		for (int i = 0; i < N; i++) {
 
+			st = new StringTokenizer(br.readLine());
+
+			int nums = Integer.parseInt(st.nextToken());
+			int target = Integer.parseInt(st.nextToken());
+
+			st = new StringTokenizer(br.readLine());
+
+			Queue<int[]> queue = new LinkedList<>();
+			PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
+			
+			for (int j = 0; j < nums; j++) {
+				int p = Integer.parseInt(st.nextToken());
+				queue.add(new int[]{j, p});
+				q.add(p);
+			}
+
+			int index = 1;
+
+			while (!queue.isEmpty()) {
+
+				int[] now = queue.poll();
+
+				if (q.peek() == now[1]) {
+
+					q.poll();
+					if (target == now[0]) {
+						System.out.println(index);
+						break;
+					}
+					index++;
+				
+				} else{
+					queue.add(now);
+				}
+			}
+		}
 
 	}
 }
