d113c4d97235cbb299801e7053805517aa3cc5b5
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-05-18 23:26:42 +0900

    18052022

--- src/backjoon/b18052022.java
+++ src/backjoon/b18052022.java
@@ -8,11 +8,41 @@ import java.util.*;
 
 public class b18052022 {
 
+	static int N;
+	static int target;
+	static int[] nums;
+	static StringBuilder sb = new StringBuilder();
+
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
 
+		N = Integer.parseInt(st.nextToken());
+		target = Integer.parseInt(st.nextToken());
+		nums = new int[target];
+
+		recursion(0);
+
+		System.out.println(sb);
+
+	}
+
+	public static void recursion(int index) {
+
+		if (index == target) {
+
+			for (int i = 0; i < nums.length; i++) {
+				sb.append(nums[i]).append(" ");
+			}
+
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = 1; i <= N; i++) {
+			nums[index] = i;
+			recursion(index + 1);
+		}
 	}
 }
