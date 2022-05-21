9c4d287c773e463f8aa121ae8ebc3c3fb2fffd41
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-05-21 23:11:29 +0900

    21052022

---
+++ src/backjoon/b21052022.java
@@ -0,0 +1,21 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//쇠막대기
+//21052022
+
+public class b21052022 {
+
+	public static void main(String[] args) throws IOException {
+
+		Scanner sc = new Scanner(System.in);
+
+		String pipes = sc.nextLine();
+
+	
+		sc.close();
+
+	}
+}
