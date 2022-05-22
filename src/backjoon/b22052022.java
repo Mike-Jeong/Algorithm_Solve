71cc2375375e98117aeb3d542293561d336d397f
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-05-22 23:00:05 +0900

    22052022

---
+++ src/backjoon/b22052022.java
@@ -0,0 +1,22 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//제곱수의 합
+//22052022
+
+public class b22052022 {
+
+	public static void main(String[] args) throws IOException {
+
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+
+		
+
+		sc.close();
+
+	}
+}
