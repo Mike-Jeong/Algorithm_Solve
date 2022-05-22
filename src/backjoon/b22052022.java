09eed88ebcfa16cb8ebc6fb33f7b4c25c975ca9d
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-05-22 23:29:19 +0900

    22052022

--- src/backjoon/b22052022.java
+++ src/backjoon/b22052022.java
@@ -14,9 +14,20 @@ public class b22052022 {
 
 		int N = sc.nextInt();
 
+		int d[]= new int[N+1];
+
+        //d[n] = min(d[n - i^2] + 1)
+        for (int i = 1; i <= N; i++){
+            d[i] = i;
+            for (int j = 1; j*j <= i; j++){
+                if (d[i] > d[i-j*j]+1){
+                    d[i] = d[i-j*j]+1;
+                }
+            }
+        }
 		
+        System.out.println(d[N]);
 
 		sc.close();
-
-	}
+    }
 }
