2240767f6107d87941587edf39215f3a8efff8ea
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-05-23 22:47:37 +0900

    23052022

--- src/backjoon/b23052022.java
+++ src/backjoon/b23052022.java
@@ -3,14 +3,32 @@ package backjoon;
 import java.io.*;
 import java.util.*;
 
-//색종이 만들기
+//치킨 TOP N
 //23052022
 
 public class b23052022 {
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int num = Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int round = 1;
+        int group_a = (int) Math.ceil((double) a / 2);
+        int group_b = (int) Math.ceil((double) b / 2);
+
+        while (group_a != group_b) {
+
+            group_a = (int) Math.ceil((double) group_a / 2);
+            group_b = (int) Math.ceil((double) group_b / 2);
+            round++;
+        }
+
+        System.out.println(round);
+
     }
 }
