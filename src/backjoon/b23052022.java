dbf2169576c5be5a289fd530583e9e2de503a43e
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-05-23 22:21:33 +0900

    23052022

---
+++ src/backjoon/b23052022.java
@@ -0,0 +1,16 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//색종이 만들기
+//23052022
+
+public class b23052022 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+    }
+}
