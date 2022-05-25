9baf6f8e24f06e7043d1294d207be5ba08a5dff3
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-05-25 20:27:48 +0900

    25052022

--- src/backjoon/b25052022.java
+++ src/backjoon/b25052022.java
@@ -12,8 +12,31 @@ public class b25052022 {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuffer sb = new StringBuffer("");
 
-        
+        int num = Integer.parseInt(st.nextToken());
+        int iter = Integer.parseInt(st.nextToken());
+
+        int[] nums = new int[num + 1];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i - 1] + Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < iter; i++) {
+
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            int count = nums[end] - nums[start - 1];
+
+            sb.append(count + "\n");
+        }
+
+        System.out.println(sb);
 
     }
 
