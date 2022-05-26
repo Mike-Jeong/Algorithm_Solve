73a2cc465c533237276ad5d358ef0baacbea02d5
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-05-26 16:42:09 +0900

    26052022

--- src/backjoon/b26052022.java
+++ src/backjoon/b26052022.java
@@ -13,7 +13,36 @@ public class b26052022 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
        
+        int num = Integer.parseInt(st.nextToken());
+        int target = Integer.parseInt(st.nextToken());
+        int count = 0;
 
+        int[] nums = new int[num + 1];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i - 1] + Integer.parseInt(st.nextToken());    
+        }
+
+        for (int i = 1; i < nums.length; i++) {
+
+            int sum = nums[i];
+
+            for (int j = i - 1; j > -1; j--) {
+
+                int now = sum - nums[j];
+
+                if (now >= target) {
+                    if (now == target) {
+                        count++;
+                    }
+                    break;
+                }
+            }
+        }
+
+        System.out.println(count);
     }
 
 }
