e78b0d9d417e78f3d061ac40629526e9156caf84
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-05-28 01:01:23 +0900

    leet 842 해결 완

--- src/programmers/p26052022.java
+++ src/programmers/p26052022.java
@@ -11,29 +11,33 @@ public class p26052022 {
 
         Solution111 s = new Solution111();
 
-        int[][] a = { { 0, 0, 0, 0, 1, 1, 1, 1 },
-                { 0, 0, 0, 0, 1, 1, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 1, 1 },
-                { 0, 0, 0, 0, 0, 0, 1, 1 },
-                { 1, 1, 0, 0, 0, 0, 0, 0 },
-                { 1, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 1 } };
-        int[] b = { 5, 4, 3, 1 };
-        int[] c = { 4, 6, 2, 8 };
-
-        System.out.println(s.solution("5015153045"));
+        System.out.println(s.solution("145670126196322518840135821983556575493101506424374"));
+        System.out.println("---------------------------------------------------------");
+        System.out.println(s.solution("49116165281446727117319003073497380461301921065340845514989233144382233615377997611611"));
+        System.out.println("---------------------------------------------------------");
+        System.out.println(s.solution("00000000000000000000"));
+        System.out.println("---------------------------------------------------------");
+        System.out.println(s.solution("010102030508013021034055089014402330377061009870159702584041811"));
+        System.out.println("---------------------------------------------------------");
+        System.out.println(s.solution("101001102103205308501380223036105840945015290247404003064770104800169570274370443940"));
+        System.out.println("---------------------------------------------------------");
+        System.out.println(s.solution("14152944"));
+        System.out.println("---------------------------------------------------------");
+        System.out.println(s.solution("1501515304575"));
+        System.out.println("---------------------------------------------------------");
 
     }
 }
 
 class Solution111 {
 
-    String s;
+    int max = Integer.MAX_VALUE;
 
     public int[] solution(String nums) {
 
         List<Integer> res = new ArrayList<>();
+
+        recursion(res, nums);
 
         int[] answer = new int[res.size()];
 
@@ -44,6 +48,49 @@ class Solution111 {
         return answer;
     }
 
-    
+    void recursion(List<Integer> list, String s) {
+
+        if (s.equals("") && list.size() > 2 && check(list)) {
+
+            for (int i = 0; i < list.size(); i++) {
+                System.out.println(list.get(i));
+            }
+
+            return;
+        }
+
+        for (int i = 1; i <= s.length(); i++) {
+
+            if (s.startsWith("0") && i > 1) {
+                return;
+            }
+
+            List<Integer> newlist = new ArrayList<>(list);
+
+            long num = Long.parseLong(s.substring(0, i));
+
+            if (num > max) {
+                return;
+            }
+
+            newlist.add((int)num);
+
+            if (check(list)) {
+                recursion(newlist, s.substring(i));
+            }
+        }
+    }
+
+    boolean check(List<Integer> list){
+
+        if (list.size() < 3) {
+            return true;
+        }
+
+        int size = list.size() - 1;
+
+        return list.get(size) == list.get(size - 1) + list.get(size - 2);
+
+    }
 
 }
