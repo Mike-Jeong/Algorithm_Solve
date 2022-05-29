a6b9e86d95ade6214586c92f5bc0c1271df8aba9
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-05-29 22:27:07 +0900

    29052022

--- src/backjoon/b29052022.java
+++ src/backjoon/b29052022.java
@@ -10,11 +10,28 @@ public class b29052022 {
 
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
 
-       
+        for (int tc = 1; tc <= T; tc++) {
+            int n = sc.nextInt();
+            Map<String, Integer> map = new HashMap<>();
 
+            for (int i = 0; i < n; i++) {
+                String name = sc.next(), type = sc.next();
+                if (name.equals("")) {
+                    
+                }
+                map.put(type, map.getOrDefault(type, 0) + 1);
+            }
+
+            int ans = 1;
+            for (int val : map.values())
+                ans *= val + 1;
+            System.out.println(ans - 1);
+        }
+
+        sc.close();
     }
-
 }
+          
