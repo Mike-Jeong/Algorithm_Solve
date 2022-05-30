d9eabfdc112a069a84585f3c182ad51ef81b1e21
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-05-30 22:42:26 +0900

    30052022

---
+++ src/backjoon/b30052022.java
@@ -0,0 +1,21 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//DFS와 BFS
+//30052022
+
+public class b30052022 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+       
+    }
+}
+          
