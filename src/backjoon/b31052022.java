5318297aa6c7fef5e3785d0c174b9d608c403de9
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-05-31 01:11:39 +0900

    31052022

---
+++ src/backjoon/b31052022.java
@@ -0,0 +1,32 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//가장 긴 증가하는 부분 수열
+//31052022
+
+public class b31052022 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        int[] dp = new int[N];
+        int[] dp = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            
+        }
+
+
+    }
+
+   
+
+}
