6d42d07e0ff60b3909d7996e04da6872f9ec3b61
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-06-01 22:42:11 +0900

    01062022

---
+++ src/backjoon/b01062022.java
@@ -0,0 +1,20 @@
+package backjoon;
+
+//import java.io.*;
+import java.util.*;
+
+//
+//01062022
+
+public class b01062022 {
+
+	public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        char input = sc.next().charAt(0);
+        
+        int num = (int) input;
+        
+        System.out.println(num);
+		sc.close();
+    }
+}
