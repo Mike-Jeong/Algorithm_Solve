5e46f7340911c37267511bbfb4c4d6a9eecd9c2b
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-06-02 23:08:12 +0900

    02062022

--- src/backjoon/b01062022.java
+++ src/backjoon/b01062022.java
@@ -3,18 +3,18 @@ package backjoon;
 //import java.io.*;
 import java.util.*;
 
-//
+//아스키코드
 //01062022
 
 public class b01062022 {
 
-	public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        char input = sc.next().charAt(0);
-        
-        int num = (int) input;
-        
-        System.out.println(num);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char input = sc.next().charAt(0);
+
+		int num = (int) input;
+
+		System.out.println(num);
 		sc.close();
-    }
+	}
 }
---
+++ src/backjoon/b02062022.java
@@ -0,0 +1,15 @@
+package backjoon;
+
+//import java.io.*;
+//import java.util.*;
+
+//등록
+//02062022
+
+public class b02062022 {
+
+  public static void main(String[] args) {
+    System.out.println(71);
+    System.out.println("minnz");
+  }
+}
