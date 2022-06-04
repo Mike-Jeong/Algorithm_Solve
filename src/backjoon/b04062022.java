47c5b53f1e4e9737b9864032e5c7e1a132fcd927
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-06-04 19:56:50 +0900

    04062022

--- src/backjoon/b04062022.java
+++ src/backjoon/b04062022.java
@@ -13,7 +13,27 @@ public class b04062022 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-   
+    int N = Integer.parseInt(st.nextToken());
+
+    long[] fabo = new long[70];
+
+    fabo[0] = 1;
+    fabo[1] = 1;
+    fabo[2] = 2;
+    fabo[3] = 4;
+
+    for (int i = 0; i < N; i++) {
+
+      st = new StringTokenizer(br.readLine());
+      int num = Integer.parseInt(st.nextToken());
+
+      for (int j = 4; j <= num; j++) {
+        fabo[j] = fabo[j - 1] + fabo[j - 2] + fabo[j - 3] + fabo[j - 4];
+      }
+
+      System.out.println(fabo[num]);
+      
+    }
 		
 	}
  
