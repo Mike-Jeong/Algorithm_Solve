05e7681898138ee19de41f7f3b67e33bb16cc476
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-06-06 21:20:54 +0900

    06062022

---
+++ src/backjoon/b06062022.java
@@ -0,0 +1,19 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//돌 게임 3
+//06062022
+
+public class b06062022 {
+
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+   
+    
+  }
+}
