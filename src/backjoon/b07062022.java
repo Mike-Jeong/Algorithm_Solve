afeb36d5bf531329d6357a5eb653ca13f51f47f0
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-06-07 15:20:03 +0900

    07062022

---
+++ src/backjoon/b07062022.java
@@ -0,0 +1,18 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//Virus outbreak
+//07062022
+
+public class b07062022 {
+
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+   
+    
+  }
+}
