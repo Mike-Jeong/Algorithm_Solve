1e96329df83f8dd1e20530e3d626acce744695b2
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-06-07 15:48:51 +0900

    07062022

--- src/backjoon/b07062022.java
+++ src/backjoon/b07062022.java
@@ -1,6 +1,7 @@
 package backjoon;
 
 import java.io.*;
+import java.math.BigInteger;
 import java.util.*;
 
 //Virus outbreak
@@ -10,9 +11,32 @@ public class b07062022 {
 
   public static void main(String[] args) throws IOException {
 
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
-   
-    
+    Scanner sc = new Scanner(System.in);
+
+    BigInteger[] dp = new BigInteger[491];
+
+    dp[1] = BigInteger.ONE;
+    dp[2] = BigInteger.ONE;
+
+    while (true) {
+
+      int num = sc.nextInt();
+
+      if (num == -1) {
+        sc.close();
+        break;
+      }
+
+      if (dp[num] != null) {
+        System.out.println("Hour " + num + ": " + dp[num] + " cow(s) affected");
+      } else {
+        for (int i = 3; i <= num; i++) {
+          dp[i] = dp[i - 1].add(dp[i - 2]); 
+        }
+
+        System.out.println("Hour " + num + ": " + dp[num] + " cow(s) affected");
+      }
+    }
+
   }
 }
