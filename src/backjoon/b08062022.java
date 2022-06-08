983c53544dd8ef422edba0e83936e25f1a3a6c17
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-06-08 11:22:42 +0900

    08062022

---
+++ src/backjoon/b08062022.java
@@ -0,0 +1,18 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//Tetrahedral Stacks of Cannonballs
+//08062022
+
+public class b08062022 {
+
+  public static void main(String[] args) throws IOException {
+
+    Scanner sc = new Scanner(System.in);
+
+   
+    
+  }
+}
