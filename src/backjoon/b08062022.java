48cdef35e71dd2470d3bd8ecf3b45d31edc18882
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-06-08 11:58:07 +0900

    08062022

--- src/backjoon/b08062022.java
+++ src/backjoon/b08062022.java
@@ -12,7 +12,29 @@ public class b08062022 {
 
     Scanner sc = new Scanner(System.in);
 
-   
+    int loop = sc.nextInt();
+
+    int[] lastballs = new int[1001];
+    int[] sums = new int[1001];
+
+    lastballs[1] = 1;
+    sums[1] = 1;
+
+    for (int i = 0; i < loop; i++) {
+      int num = sc.nextInt();
+
+      if (sums[num] == 0) {
+
+        for (int j = 2; j <= num; j++) {
+          lastballs[j] = lastballs[j - 1] + j;
+          sums[j] = sums[j-1] + lastballs[j];
+        }
+      }
+
+      System.out.println((i + 1) + ": " + num + " " + sums[num]);
+    }
+
+   sc.close();
     
   }
 }
