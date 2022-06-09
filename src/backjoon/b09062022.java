2085abb8dffa525becd2e0e58c5437d6342edea0
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-06-09 17:38:37 +0900

    09062022

--- src/backjoon/b09062022.java
+++ src/backjoon/b09062022.java
@@ -12,7 +12,20 @@ public class b09062022 {
 
     Scanner sc = new Scanner(System.in);
 
-   
-    
+    int num = sc.nextInt();
+
+    long[] dp = new long[num + 1];
+
+    dp[0] = 0;
+    dp[1] = 1;
+
+    for (int i = 2; i <= num; i++) {
+      dp[i] = dp[i -1] + dp[i -2];
+    }
+
+    System.out.println(dp[num]);
+
+    sc.close();
+
   }
 }
