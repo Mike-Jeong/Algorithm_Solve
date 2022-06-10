c8fd024814924508b821b3835de082e7f7fe531b
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-06-11 00:19:22 +0900

    10062022

--- src/backjoon/b10062022.java
+++ src/backjoon/b10062022.java
@@ -1,7 +1,7 @@
 package backjoon;
 
 import java.io.*;
-import java.util.*;
+//import java.util.*;
 
 //핸드폰 번호 궁합
 //10062022
@@ -10,11 +10,22 @@ public class b10062022 {
 
   public static void main(String[] args) throws IOException {
 
-    Scanner sc = new Scanner(System.in);
-
-    
-
-    sc.close();
-
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String s1 = br.readLine();
+    String s2 = br.readLine();
+    String s = "";
+    for (int i = 0; i < s1.length(); i++) {
+      s += s1.charAt(i);
+      s += s2.charAt(i);
+    }
+    while (s.length() > 2) {
+      String S = "";
+      for (int i = 0; i < s.length() - 1; i++) {
+        int n = Character.getNumericValue(s.charAt(i)) + Character.getNumericValue(s.charAt(i + 1));
+        S += Integer.toString(n % 10);
+      }
+      s = S;
+    }
+    System.out.print(s);
   }
 }
