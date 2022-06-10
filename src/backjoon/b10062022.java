2e1973715c5691921e1a3c32221a14c307fdae16
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-06-10 23:20:11 +0900

    10062022

---
+++ src/backjoon/b10062022.java
@@ -0,0 +1,20 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//핸드폰 번호 궁합
+//10062022
+
+public class b10062022 {
+
+  public static void main(String[] args) throws IOException {
+
+    Scanner sc = new Scanner(System.in);
+
+    
+
+    sc.close();
+
+  }
+}
