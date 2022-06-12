becd6d3ffa36173ac8c060acfed6da4307676b06
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-06-12 12:07:29 +0900

    12062022

--- src/backjoon/b12062022.java
+++ src/backjoon/b12062022.java
@@ -10,6 +10,15 @@ public class b12062022 {
 
   public static void main(String[] args) throws IOException {
 
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+
+		bw.write((N % 2 == 1) ? "SK" : "CY");
+		bw.flush();
+		bw.close();
+		br.close();
     
   }
 }
