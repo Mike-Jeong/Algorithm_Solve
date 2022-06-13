2a216455492276ae7ec2388cd4996c8e251f9c78
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-06-13 23:19:37 +0900

    13062022

--- src/backjoon/b13062022.java
+++ src/backjoon/b13062022.java
@@ -1,7 +1,7 @@
 package backjoon;
 
 import java.io.*;
-//import java.util.*;
+import java.util.*;
 
 //BABBA
 //13062022
@@ -10,7 +10,26 @@ public class b13062022 {
 
   public static void main(String[] args) throws IOException {
 
-  
+	Scanner sc = new Scanner(System.in);
+
+	int n = sc.nextInt();
+
+	int[] dpA = new int[n + 1];
+	int[] dpB = new int[n + 1];
+
+	dpA[0] = 1;
+	dpB[1] = 1;
+
+	for (int i = 2; i < dpB.length; i++) {
+
+		dpA[i] = dpB[i - 1];
+		dpB[i] = dpB[i - 1] + dpB[i-2];
+
+	}
+
+	System.out.println(dpA[n] + " " + dpB[n]);
+
+	sc.close();
 
   }
 }
