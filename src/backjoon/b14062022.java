f1ec8074c3140333e4c89784cef0f943ae086580
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-06-14 22:47:37 +0900

    14062022

--- src/backjoon/b14062022.java
+++ src/backjoon/b14062022.java
@@ -1,15 +1,29 @@
 package backjoon;
 
-import java.io.*;
+//import java.io.*;
 import java.util.*;
 
-//
+//설탕배달
 //14062022
 
 public class b14062022 {
 
-  public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 
-	
-  }
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+
+		if (N == 4 || N == 7) {
+			System.out.println(-1);
+		} else if (N % 5 == 0) {
+			System.out.println(N / 5);
+		} else if (N % 5 == 1 || N % 5 == 3) {
+			System.out.println((N / 5) + 1);
+		} else if (N % 5 == 2 || N % 5 == 4) {
+			System.out.println((N / 5) + 2);
+		}
+
+		sc.close();
+	}
 }
