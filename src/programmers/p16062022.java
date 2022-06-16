83bf0c00f87b2403ef62481011ebeb848d2796c9
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-06-16 21:08:26 +0900

    16062022

---
+++ src/programmers/p16062022.java
@@ -0,0 +1,26 @@
+package programmers;
+
+import java.util.*;
+
+//?
+//16062022
+
+public class p16062022 {
+
+    public static void main(String[] args) throws Exception {
+
+        Solution112 s = new Solution112();
+
+        int a = 0;
+        int b = 0;
+        int c = 0;
+
+        System.out.println(s.solution());
+
+    }
+}
+
+class Solution112 {
+
+   
+}
