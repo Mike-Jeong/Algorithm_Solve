eb88bbb39d188f5e2aa250d2b9421dbabe004de8
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-06-17 22:37:03 +0900

    17062022

---
+++ src/backjoon/b17062022.java
@@ -0,0 +1,16 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//
+//17062022
+
+public class b17062022 {
+
+	public static void main(String[] args) throws IOException {
+
+		
+
+	}
+}
