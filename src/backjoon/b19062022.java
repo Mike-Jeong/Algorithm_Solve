3ea6d6a30036d47a3c2ab9f6f207577467e5e1ef
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-06-19 23:58:22 +0900

    19062022

--- src/backjoon/b19062022.java
+++ src/backjoon/b19062022.java
@@ -10,8 +10,87 @@ public class b19062022 {
 
     public static void main(String[] args) throws IOException {
 
-        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
+        int n = Integer.parseInt(st.nextToken());
+
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+
+        int[] dp = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < dp.length; i++) {
+
+            int current = Integer.parseInt(st.nextToken());  
+
+            min = Math.min(min, current);
+
+            max = Math.max(max, current - min);
+
+            dp[i] = max;
+
+
+            sb.append(dp[i]);
+
+            if (i != n - 1) {
+                sb.append(" ");
+            }
+        }
+
+        System.out.println(sb.toString());
     }
 }
 
+/*   public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int before = -1;
+
+        int[] dp = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < dp.length; i++) {
+
+            int current = Integer.parseInt(st.nextToken());  
+
+            if (current < min && current < before) {
+                dp[i] = dp[i-1];
+                min = current;
+                before = current;
+                max = current;
+                sb.append(dp[i] + " ");
+                continue;
+            }
+            
+            if (current < min) {
+                min = current;
+            }
+
+            if (current > max) {
+                max = current;
+            }
+
+            dp[i] = max - min;
+            before = current;
+
+            sb.append(dp[i] + " ");
+
+            if (i != n - 1) {
+                sb.append(" ");
+            }
+        }
+
+        System.out.println(sb.toString());
+    }*/
