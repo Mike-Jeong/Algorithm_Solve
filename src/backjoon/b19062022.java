6137bd996e565db725982ac13c59a7faef1d0af5
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-06-19 22:57:36 +0900

    19062022

---
+++ src/backjoon/b19062022.java
@@ -0,0 +1,17 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//크림 파스타
+//19062022
+
+public class b19062022 {
+
+    public static void main(String[] args) throws IOException {
+
+        
+
+    }
+}
+
