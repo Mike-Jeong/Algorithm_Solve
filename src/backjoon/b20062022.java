921974e0003f37a127569c0e8e7e9a3806836a60
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-06-20 20:35:14 +0900

    20062022

---
+++ src/backjoon/b20062022.java
@@ -0,0 +1,22 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//크림 파스타
+//20062022
+
+public class b20062022 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+      
+
+        System.out.println(sb.toString());
+    }
+}
+
