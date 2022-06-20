53a6442bfc72a107a44fcb3d5dc4a1e82633e5bd
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-06-20 23:26:06 +0900

    20062022

--- src/backjoon/b20062022.java
+++ src/backjoon/b20062022.java
@@ -8,15 +8,40 @@ import java.util.*;
 
 public class b20062022 {
 
+    static int[] arr;
+    static Integer[] dp;
+    static int max;
+
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
+        Scanner sc = new Scanner(System.in);
 
-      
+        int N = sc.nextInt();
 
-        System.out.println(sb.toString());
+        arr = new int[N];
+        dp = new Integer[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        dp[0] = arr[0];
+        max = arr[0];
+
+        recur(N - 1);
+
+        System.out.println(max);
+
+        sc.close();
+    }
+
+    static int recur(int N) {
+
+        if (dp[N] == null) {
+            dp[N] = Math.max(recur(N - 1) + arr[N], arr[N]);
+            max = Math.max(dp[N], max);
+        }
+
+        return dp[N];
     }
 }
-
