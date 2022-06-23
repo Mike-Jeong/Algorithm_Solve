cf9bb65fc66a3502a379d54e518453759d71b724
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-06-23 21:49:58 +0900

    23062022

---
+++ src/programmers/p23062022.java
@@ -0,0 +1,30 @@
+package programmers;
+
+import java.util.*;
+
+//?
+//23062022
+
+public class p23062022 {
+
+    public static void main(String[] args) throws Exception {
+
+        Solution113 s = new Solution113();
+
+        int a = 4;
+        int[][] b = { { 0, 1 }, { 1, 3 } };
+        int[][] c = { { 0, 2 } };
+
+        System.out.println(s.solution(a, b, c));
+
+    }
+}
+
+class Solution113 {
+
+    public int[] solution(int N, int[][] left, int[][] right) {
+
+        return new int[] {};
+    }
+
+}
