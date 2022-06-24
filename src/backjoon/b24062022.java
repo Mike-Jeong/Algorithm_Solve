c09139c5a7d58a50687ed163f4844842c15097cf
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-06-24 23:59:33 +0900

    24062022

--- src/backjoon/b24062022.java
+++ src/backjoon/b24062022.java
@@ -10,7 +10,34 @@ public class b24062022 {
 
     public static void main(String[] args) throws IOException {
 
-      
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] soldiers =new int[N];
+        int[] dp = new int[N+1];
+
+        Arrays.fill(dp,1);
+        String temp = br.readLine();
+        StringTokenizer st = new StringTokenizer(temp);
+        int k = 0;
+        while (st.hasMoreTokens()){
+            soldiers[k++]=Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=1;i<N;i++){
+            for(int j=0;j<=i;j++){
+                if(soldiers[i]<soldiers[j]){
+                    dp[i]=Math.max(dp[i],dp[j]+1);
+                }
+            }
+        }
+        int max = 0;
+        Arrays.sort(dp);
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(Integer.toString(N-dp[N]));
+        bw.flush();
+        bw.close();
 
     }
 
