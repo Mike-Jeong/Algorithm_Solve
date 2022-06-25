d967f90e52590143fa9a5c67b59752e0960a3a86
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-06-25 23:00:01 +0900

    25062022

--- src/backjoon/b24062022.java
+++ src/backjoon/b24062022.java
@@ -3,7 +3,7 @@ package backjoon;
 import java.io.*;
 import java.util.*;
 
-//
+//병사 배치하기
 //24062022
 
 public class b24062022 {
@@ -11,33 +11,37 @@ public class b24062022 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(br.readLine());
-        int[] soldiers =new int[N];
-        int[] dp = new int[N+1];
+        int n = Integer.parseInt(st.nextToken());
 
-        Arrays.fill(dp,1);
-        String temp = br.readLine();
-        StringTokenizer st = new StringTokenizer(temp);
-        int k = 0;
-        while (st.hasMoreTokens()){
-            soldiers[k++]=Integer.parseInt(st.nextToken());
-        }
+        int[] dp = new int[n + 1];
+        int[] num = new int[n + 1];
 
-        for(int i=1;i<N;i++){
-            for(int j=0;j<=i;j++){
-                if(soldiers[i]<soldiers[j]){
-                    dp[i]=Math.max(dp[i],dp[j]+1);
-                }
-            }
-        }
+        st = new StringTokenizer(br.readLine());
+
         int max = 0;
-        Arrays.sort(dp);
 
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.write(Integer.toString(N-dp[N]));
-        bw.flush();
-        bw.close();
+        for (int i = 1; i < dp.length; i++) {
+
+            dp[i] = 1;
+            num[i] = Integer.parseInt(st.nextToken());
+
+            for (int j = i - 1; j >= 1; j--) {
+
+                if (num[i] < num[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+
+            }
+
+            if (dp[i] > max) {
+                max = dp[i];
+            }
+
+        }
+
+        System.out.println(n - max);
 
     }
 
---
+++ src/backjoon/b25062022.java
@@ -0,0 +1,20 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//
+//25062022
+
+public class b25062022 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+       
+
+    }
+
+}
