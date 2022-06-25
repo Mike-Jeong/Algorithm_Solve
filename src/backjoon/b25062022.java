ca42741494e5c870513112b9e689a707db6a8729
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-06-25 23:03:06 +0900

    25062022

--- src/backjoon/b25062022.java
+++ src/backjoon/b25062022.java
@@ -3,7 +3,7 @@ package backjoon;
 import java.io.*;
 import java.util.*;
 
-//
+//가장 긴 감소하는 부분 수열
 //25062022
 
 public class b25062022 {
@@ -13,8 +13,35 @@ public class b25062022 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-       
+        int n = Integer.parseInt(st.nextToken());
 
+        int[] dp = new int[n + 1];
+        int[] num = new int[n + 1];
+
+        st = new StringTokenizer(br.readLine());
+
+        int max = 0;
+
+        for (int i = 1; i < dp.length; i++) {
+
+            dp[i] = 1;
+            num[i] = Integer.parseInt(st.nextToken());
+
+            for (int j = i - 1; j >= 1; j--) {
+
+                if (num[i] < num[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+
+            }
+
+            if (dp[i] > max) {
+                max = dp[i];
+            }
+
+        }
+
+        System.out.println(max);
     }
 
 }
