a4bdae8755ff5a63956255c06632efec20c20aa4
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-06-27 23:42:21 +0900

    27062022

---
+++ src/backjoon/b27062022.java
@@ -0,0 +1,15 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//연속부분최대곱
+//27062022
+
+public class b27062022 {
+
+    public static void main(String[] args) throws IOException {
+
+    }
+
+}
