b661b12a35198f3e9653ef7b8f760ac61d4bc431
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-06-27 23:43:28 +0900

    27062022

--- src/backjoon/b27062022.java
+++ src/backjoon/b27062022.java
@@ -1,7 +1,7 @@
 package backjoon;
 
 import java.io.*;
-import java.util.*;
+//import java.util.*;
 
 //연속부분최대곱
 //27062022
@@ -10,6 +10,23 @@ public class b27062022 {
 
     public static void main(String[] args) throws IOException {
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        double[] arr = new double[n + 1];
+        double[] dp = new double[n + 1];
+        double max = 0;
+
+        for (int i = 1; i <= n; i++) {
+
+            arr[i] = Double.parseDouble(br.readLine());
+            dp[i] = Math.max(arr[i], dp[i - 1] * arr[i]);
+
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.print(String.format("%.3f", max));
     }
 
 }
