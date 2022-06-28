895cd486e0c8ba1a0b04f64b85a53ab4f3a06b53
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-06-28 20:42:13 +0900

    28062022

--- src/backjoon/b28062022.java
+++ src/backjoon/b28062022.java
@@ -1,7 +1,7 @@
 package backjoon;
 
 import java.io.*;
-//import java.util.*;
+import java.util.*;
 
 //분산처리
 //28062022
@@ -10,7 +10,34 @@ public class b28062022 {
 
     public static void main(String[] args) throws IOException {
 
-        
+        Scanner s = new Scanner(System.in);
+        int T = s.nextInt();
+        for (int j = 0; j < T; j++) {
+            int a = s.nextInt();
+            int b = s.nextInt();
+            int c = 0;
+            int answer = 1;
+
+            if (a % 10 == 0 || a % 10 == 1 || a % 10 == 5 || a % 10 == 6) {
+                answer = a % 10;
+            } else if (a % 10 == 4 || a % 10 == 9) {
+                c = b % 2;
+                if (c == 0)
+                    c = 2;
+            } else {
+                c = b % 4;
+                if (c == 0)
+                    c = 4;
+            }
+
+            for (int i = 0; i < c; i++)
+                answer = (answer * a) % 10;
+            if (answer == 0)
+                answer = 10;
+            System.out.println(answer);
+        }
+        s.close();
+
     }
 
 }
