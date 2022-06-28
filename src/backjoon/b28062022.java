8b2433777ea0f486d45b66ca2bfb9f467e329658
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-06-28 20:07:32 +0900

    28062022

---
+++ src/backjoon/b28062022.java
@@ -0,0 +1,16 @@
+package backjoon;
+
+import java.io.*;
+//import java.util.*;
+
+//분산처리
+//28062022
+
+public class b28062022 {
+
+    public static void main(String[] args) throws IOException {
+
+        
+    }
+
+}
