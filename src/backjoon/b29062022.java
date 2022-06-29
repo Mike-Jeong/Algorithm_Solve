bc2cdcc9c3c49fe1d799f80ec4ab57205f008a60
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-06-29 23:57:54 +0900

    29062022

--- src/backjoon/b29062022.java
+++ src/backjoon/b29062022.java
@@ -3,15 +3,38 @@ package backjoon;
 import java.io.*;
 import java.util.*;
 
-//
+//게임
 //29062022
 
 public class b29062022 {
 
     public static void main(String[] args) throws IOException {
 
-       
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z = getPercent(x, y);
+
+        int ans = -1;
+        int left = 0;
+        int right = (int) 1e9;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (getPercent(x + mid, y + mid) != z) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        System.out.println(ans);
+        sc.close();
 
     }
 
+    static int getPercent(int x, int y) {
+        return (int) ((long) y * 100 / x);
+    }
 }
