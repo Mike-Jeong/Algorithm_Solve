dbb18c1308e82755567e6ccdb404aed3521950dd
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-07-02 00:02:52 +0900

    01072022

--- src/backjoon/b01072022.java
+++ src/backjoon/b01072022.java
@@ -8,10 +8,53 @@ import java.util.*;
 
 public class b01072022 {
 
-    public static void main(String[] args) throws IOException {
+	static int count = 0;
+	static boolean[] visited;
+	static boolean[][] conn;
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws IOException {
 
-    }
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int n_con = Integer.parseInt(st.nextToken());
+
+		visited = new boolean[n + 1];
+
+		conn = new boolean[n + 1][n + 1];
+
+		for (int i = 0; i < n_con; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			int r = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+
+			conn[r][c] = true;
+			conn[c][r] = true;
+		}
+
+		for (int i = 1; i <= n; i++) {
+
+			if (!visited[i]) {
+				count++;
+				dfs(i);
+			}
+
+		}
+
+		System.out.println(count);
+
+	}
+
+	static void dfs(int node) {
+
+		visited[node] = true;
+
+		for (int i = 1; i < visited.length; i++) {
+			if (!visited[i] && conn[node][i]) {
+				dfs(i);
+			}
+		}
+	}
 }
