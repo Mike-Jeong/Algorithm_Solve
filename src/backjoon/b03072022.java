03d7c660fedd5c2cf7cef1fc67a73ce4faaeffe9
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-07-03 22:49:31 +0900

    03072022

---
+++ src/backjoon/b03072022.java
@@ -0,0 +1,18 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//택배 배송
+//03072022
+
+public class b03072022 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+	}
+
+}
