0fee07e3edace0321b8fba7acd75873137cf8912
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-07-03 23:25:46 +0900

    03072022

--- src/backjoon/b03072022.java
+++ src/backjoon/b03072022.java
@@ -8,11 +8,86 @@ import java.util.*;
 
 public class b03072022 {
 
+	static boolean[] visited;
+	static Map<Integer, ArrayList<Node>> map = new HashMap<>();
+	static int[] dp;
+
 	public static void main(String[] args) throws IOException {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		visited = new boolean[n + 1];
+		dp = new int[n + 1];
+
+		Arrays.fill(dp, 987654321);
+
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+
+			if (!map.containsKey(s)) {
+				map.put(s, new ArrayList<Node>());
+			}
+
+			map.get(s).add(new Node(e, w));
+
+			if (!map.containsKey(e)) {
+				map.put(e, new ArrayList<Node>());
+			}
+
+			map.get(e).add(new Node(s, w));
+		}
+
+		PriorityQueue<Node> queue = new PriorityQueue<>();
+
+		queue.add(new Node(1, 0));
+		dp[1] = 0;
+
+		while (!queue.isEmpty()) {
+
+			Node current = queue.poll();
+
+			if (!visited[current.end]) {
+
+				visited[current.end] = true;
+
+				ArrayList<Node> cNodes = map.get(current.end);
+
+				for (int i = 0; i < cNodes.size(); i++) {
+					Node next = cNodes.get(i);
+
+					if(dp[next.end] > dp[current.end] + next.weight) {
+						dp[next.end] = dp[current.end] + next.weight;
+						queue.add(new Node(next.end, dp[next.end]));
+					}
+				}
+			}
+		}
+
+		System.out.println(dp[n]);
+
+	}
+}
+
+class Node implements Comparable<Node> {
+	int end;
+	int weight;
+
+	Node(int end, int weight) {
+		this.end = end;
+		this.weight = weight;
+	}
+
+	@Override
+	public int compareTo(Node o) {
+		return weight - o.weight;
 	}
 
 }
