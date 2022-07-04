303493d9a88c0e51d7f636f04775e8620f882e39
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-07-04 22:11:45 +0900

    04072022

--- src/backjoon/b04072022.java
+++ src/backjoon/b04072022.java
@@ -13,7 +13,48 @@ public class b04072022 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		
+		int n = Integer.parseInt(st.nextToken());
 
+		int[] request = new int[n];
+
+		int left = 0;
+		int right = 0;
+
+		st = new StringTokenizer(br.readLine());
+		
+		for (int i = 0; i < request.length; i++) {
+
+			
+			request[i] = Integer.parseInt(st.nextToken());
+
+			right = Math.max(right, request[i]);
+		}
+
+		st = new StringTokenizer(br.readLine());
+
+		int max = Integer.parseInt(st.nextToken());
+
+		while (left <= right) {
+
+			int mid = (left + right) / 2;
+
+			int sum = 0;
+
+			for (int i = 0; i < request.length; i++) {
+				if (request[i] > mid) {
+					sum += mid;
+				} else {
+					sum += request[i];
+				}
+			}
+
+			if (sum <= max) {
+				left = mid + 1;
+			} else{
+				right = mid - 1;
+			}
+		}
+
+		System.out.println(right);
 	}
 }
