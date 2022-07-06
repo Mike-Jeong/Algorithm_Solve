d576dd6cf4f830eb435521d864020245389bbeff
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-07-06 23:47:25 +0900

    06072022

--- src/backjoon/b06072022.java
+++ src/backjoon/b06072022.java
@@ -8,15 +8,51 @@ import java.util.*;
 
 public class b06072022 {
 
+	static int min = Integer.MAX_VALUE;
+    static int n, m;
+    static boolean[] visited;
+    static int max = 100000;
+
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+
+		visited = new boolean[max + 1];
+        bfs();
+        System.out.println(min);
 
 		
-
 	}
+
+	public static void bfs() {
+
+        Queue<Node> q = new LinkedList<>();
+        q.offer(new Node(n, 0));
+        
+        while(!q.isEmpty()) {
+            Node node = q.poll();
+            visited[node.x] = true;
+            if(node.x == m) min = Math.min(min, node.time);
+            
+            if(node.x * 2 <= max && visited[node.x * 2] == false) q.offer(new Node(node.x * 2, node.time));
+            if(node.x + 1 <= max && visited[node.x + 1] == false) q.offer(new Node(node.x + 1, node.time + 1));
+            if(node.x - 1 >= 0 && visited[node.x - 1] == false) q.offer(new Node(node.x - 1, node.time + 1));
+        }
+    }
+
+	public static class Node {
+        int x;
+        int time;
+        
+        public Node(int x, int time) {
+            this.x = x;
+            this.time = time;
+        }
+    }
+
 }
+
