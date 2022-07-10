4173834b6609fda3caa30f0478a46d33bb7e4198
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-07-10 22:46:30 +0900

    10072022

---
+++ src/backjoon/b10072022.java
@@ -0,0 +1,19 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//특정 거리의 도시 찾기
+//10072022
+
+public class b10072022 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        
+    }
+}
+
