c251d673b93d867cc9f7946d1dc9305abcd37d45
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-07-11 23:04:12 +0900

    11072022

---
+++ src/backjoon/b11072022.java
@@ -0,0 +1,18 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//녹색 옷 입은 애가 젤다지?
+//11072022
+
+public class b11072022 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        
+    }
+}
