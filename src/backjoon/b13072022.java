2a2d65b20753bcbcbd82cfe501245d64b946a82a
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-07-13 22:52:54 +0900

    13072022

---
+++ src/backjoon/b13072022.java
@@ -0,0 +1,17 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//랜선 자르기
+//13072022
+
+public class b13072022 {
+
+    public static void main(String args[]) throws Exception
+    {
+        
+    }
+
+        
+}
