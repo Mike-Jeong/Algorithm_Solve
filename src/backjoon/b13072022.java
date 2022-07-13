2528e98f3af523c61758fe8adbd746ef1c8fb05e
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-07-13 23:25:11 +0900

    13072022

--- src/backjoon/b13072022.java
+++ src/backjoon/b13072022.java
@@ -8,10 +8,43 @@ import java.util.*;
 
 public class b13072022 {
 
-    public static void main(String args[]) throws Exception
-    {
-        
-    }
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        
+        int n = Integer.parseInt(st.nextToken());
+        int target = Integer.parseInt(st.nextToken());
+
+        int[] lenght = new int[n];
+
+        long right = 0;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            lenght[i] = Integer.parseInt(st.nextToken());
+
+            right = Math.max(right, lenght[i]);
+        }
+
+        long left = 1;
+        long mid = 0;
+
+        while (left <= right) {
+
+            long count = 0;
+            mid = (left + right) / 2;
+
+            for (int i = 0; i < lenght.length; i++) {
+                count += (lenght[i] / mid);
+            }
+
+            if (count < target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        System.out.println(right);
+
+    }
 }
