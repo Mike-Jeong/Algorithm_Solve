a63fed53f737caf58c3a22d4d21dbd752abad42d
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-07-14 22:14:04 +0900

    14072022

--- src/backjoon/b14072022.java
+++ src/backjoon/b14072022.java
@@ -8,11 +8,47 @@ import java.util.*;
 
 public class b14072022 {
 
+    static Integer[][] dp;
+    static int[] w; 
+	static int[] v; 
+
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-    }
+        w = new int[n];
+		v = new int[n];
+ 
+		dp = new Integer[n][k + 1];
+ 
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			w[i] = Integer.parseInt(st.nextToken());
+			v[i] = Integer.parseInt(st.nextToken());
+		}
+ 
+		System.out.println(knapsack(n - 1, k));
+ 
+	}
+ 
+	static int knapsack(int i, int k) {
+
+		if (i < 0)
+			return 0;
+		
+		if (dp[i][k] == null) {
+			
+			if(w[i] > k) {
+				dp[i][k] = knapsack(i - 1, k);
+			}
+			
+			else {
+				dp[i][k] = Math.max(knapsack(i - 1, k), knapsack(i - 1, k - w[i]) + v[i]);
+			}
+		}
+		return dp[i][k];
+	}
 }
