96c3f82ab89024ba5c256425a7aa4eba4893a8eb
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-07-19 20:33:49 +0900

    19072022

--- src/backjoon/b19072022.java
+++ src/backjoon/b19072022.java
@@ -1,16 +1,44 @@
 package backjoon;
 
-import java.io.*;
+//import java.io.*;
 import java.util.*;
 
-//
+//베르트랑 공준
 //19072022
 
 public class b19072022 {
 
 	public static void main(String args[]) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+			int n = sc.nextInt();
+
+			if (n == 0) {
+				sc.close();
+				break;
+			}
+
+			int lastNum = n * 2 + 1;
+			boolean[] primeCk = new boolean[lastNum];
+
+			for (int i = 2; i <= Math.sqrt(lastNum); i++) {
+				for (int j = i + i; j < lastNum; j += i) {
+					primeCk[j] = true;
+				}
+			}
+
+			int count = 0;
+
+			for (int i = n + 1; i < primeCk.length; i++) {
+				if (!primeCk[i]) {
+					count++;
+				}
+			}
+
+			System.out.println(count);
+		}
 
 	}
 
