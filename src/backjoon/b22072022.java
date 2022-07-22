b8327822ca38c4a9848cefd7285fd419704e7cb9
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-07-23 02:13:08 +0900

    22072022

--- src/backjoon/b22072022.java
+++ src/backjoon/b22072022.java
@@ -8,11 +8,50 @@ import java.util.*;
 
 public class b22072022 {
 
+    static int[][] dp; 
+
+    public static void draw(){
+
+        System.out.println("-------------------------------");
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+    }
+
 	public static void main(String args[]) throws Exception {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		
+		int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[][] items = new int[n + 1][2];
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            items[i][0] = Integer.parseInt(st.nextToken());
+            items[i][1] = Integer.parseInt(st.nextToken());
+
+        }
+
+        dp = new int[n + 1][k + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= k; j++) {
+                if (items[i][0] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - items[i][0]] + items[i][1]);
+                }
+
+                draw();
+            }
+        }
+
     }
 }
