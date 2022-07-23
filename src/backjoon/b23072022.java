18b3849d7aeac8718eafb25f1ad9057c051d44c1
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-07-23 22:44:09 +0900

    23072022

---
+++ src/backjoon/b23072022.java
@@ -0,0 +1,19 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//창영이와 커피
+//23072022
+
+public class b23072022 {
+
+	public static void main(String args[]) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		
+
+    }
+}
