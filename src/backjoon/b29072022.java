dfcbad98ae58c09e4abcfe81f63301b5c878c3f7
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-07-29 23:22:45 +0900

    29072022

--- src/backjoon/b29072022.java
+++ src/backjoon/b29072022.java
@@ -13,5 +13,25 @@ public class b29072022 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int n = Integer.parseInt(st.nextToken());
+
+        Stack<int[]> stack = new Stack<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            int v = Integer.parseInt(st.nextToken());
+			while (!stack.isEmpty()) {
+				if (stack.peek()[1] >= v) {
+					System.out.print(stack.peek()[0] + " ");
+					break;
+				}
+				stack.pop();
+			}
+			if (stack.isEmpty()) {
+				System.out.print("0 ");
+			}
+			stack.push(new int[] { i, v });
+        }
+
     }
 }
