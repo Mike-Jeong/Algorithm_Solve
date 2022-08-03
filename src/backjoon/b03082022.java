1ef85cc93dd1ae6de674c355d5012d466a8b1d6d
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-08-03 22:24:09 +0900

    03082022

---
+++ src/backjoon/b03082022.java
@@ -0,0 +1,18 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//최소비용 구하기 2
+//03082022
+
+public class b03082022 {
+
+	public static void main(String args[]) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		
+	}
+}
