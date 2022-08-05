f7f18be98cb0ae02055fba8670c0db685831d784
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-08-05 22:58:33 +0900

    05082022

--- src/backjoon/b05082022.java
+++ src/backjoon/b05082022.java
@@ -3,17 +3,48 @@ package backjoon;
 import java.io.*;
 import java.util.*;
 
-//톱니바퀴
+//1학년
 //05082022
 
 public class b05082022 {
 
+    static int n;
+    static int[] nums;
+    static long[][] dp;
 
-	public static void main(String args[]) throws Exception {
+    public static void main(String args[]) throws Exception {
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	}
+        n = Integer.parseInt(st.nextToken());
+
+        nums = new int[n];
+        dp = new long[n - 1][21];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[0][nums[0]]++;
+
+        for (int i = 1; i < n - 1; i++) {
+            for (int j = 0; j < 21; j++) {
+                if (dp[i - 1][j] != 0) {
+                    int plus = j + nums[i];
+                    int minus = j - nums[i];
+                    if (plus >= 0 && plus <= 20) {
+                        dp[i][plus] += dp[i - 1][j];
+                    }
+                    if (minus >= 0 && minus <= 20) {
+                        dp[i][minus] += dp[i - 1][j];
+                    }
+                }
+            }
+        }
+
+        System.out.println(dp[n-2][nums[n-1]]);
+
+    }
 }
-
