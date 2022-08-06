1bbe19f285f547ef3bfe5b62e23b0e4e3007656c
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-08-06 23:28:38 +0900

    06082022

--- src/backjoon/b06082022.java
+++ src/backjoon/b06082022.java
@@ -3,7 +3,7 @@ package backjoon;
 import java.io.*;
 import java.util.*;
 
-//
+//타일 채우기
 //06082022
 
 public class b06082022 {
@@ -13,5 +13,30 @@ public class b06082022 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] dp = new int[n + 1];
+
+        if (n % 2 == 0) {
+
+            dp[0] = 1;
+            dp[2] = 3;
+
+            for (int i = 4; i <= n; i += 2) {
+
+                dp[i] = dp[i - 2] * dp[2];
+
+                for (int j = i - 4; j >= 0; j -= 2) {
+
+                    dp[i] += dp[j] * 2;
+                    
+                }
+            }
+
+            System.out.println(dp[n]);
+
+        } else {
+            System.out.println(0);
+        }
     }
 }
