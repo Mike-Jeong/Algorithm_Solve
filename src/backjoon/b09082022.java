08ec4d6c787c6a541882ae90878f94d97db5ea71
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-08-09 23:05:19 +0900

    09082022

---
+++ src/backjoon/b09082022.java
@@ -0,0 +1,19 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//신기한 소수
+//09082022
+
+public class b09082022 {
+
+    public static void main(String args[]) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        
+    }
+
+}
