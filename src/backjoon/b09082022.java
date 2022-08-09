77926b39f44e92b1dc5f84a28ad0732f9b3b9112
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-08-10 00:20:56 +0900

    09082022

--- src/backjoon/b09082022.java
+++ src/backjoon/b09082022.java
@@ -13,7 +13,42 @@ public class b09082022 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        
+        int n = Integer.parseInt(st.nextToken());
+
+        recursion(0, 0, n);
+
+    }
+
+    public static void recursion(int count, int num, int end) {
+
+        if (count == end) {
+            System.out.println(num);
+            return;
+        }
+
+        for (int i = 1; i <= 9; i++) {
+            int n = (num * 10) + i;
+            if (isPrime(n)) {
+                recursion(count + 1, n, end);
+            }
+        }
+    }
+
+    public static boolean isPrime(int num) {
+
+        int sqrt = (int) Math.sqrt(num);
+
+        if (num == 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= sqrt; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
 }
