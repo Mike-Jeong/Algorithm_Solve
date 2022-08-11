e9e30e182dcc756d06684965011e01bd8fee7974
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-08-12 00:22:34 +0900

    11082022

--- src/backjoon/b11082022.java
+++ src/backjoon/b11082022.java
@@ -3,7 +3,7 @@ package backjoon;
 import java.io.*;
 import java.util.*;
 
-//
+//합분해
 //11082022
 
 public class b11082022 {
@@ -12,6 +12,21 @@ public class b11082022 {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[k+1][n+1];
+        Arrays.fill(dp[1], 1);
+        for (int i = 1; i <= k; i++) dp[i][0] = 1;
+
+        for (int i = 2; i <= k; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                dp[i][j] %= 1000000000;
+            }
+        }
+
+        System.out.println(dp[k][n]);
        
     }
 }
