f731b0b2ceb8bcc8f8f6709a7eba50eaa669703e
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-08-15 22:21:26 +0900

    15082022

--- src/backjoon/b15082022.java
+++ src/backjoon/b15082022.java
@@ -3,7 +3,7 @@ package backjoon;
 import java.io.*;
 import java.util.*;
 
-//
+//로봇 프로젝트
 //15082022
 
 public class b15082022 {
@@ -11,8 +11,49 @@ public class b15082022 {
     public static void main(String args[]) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        String s = null;
 
-        
+        while ((s = br.readLine()) != null) {
+
+            int x = Integer.parseInt(s) * 10000000;
+
+            int n = Integer.parseInt(br.readLine());
+
+            int[] nums = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                nums[i] = Integer.parseInt(br.readLine());
+            }
+
+            Arrays.sort(nums);
+
+            if (n > 1) {
+                int left = 0;
+                int right = n - 1;
+
+                while (left < right) {
+
+                    int sum = nums[left] + nums[right];
+
+                    if (sum == x) {
+                        break;
+                    } else if (sum < x) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+
+                if (left == right) {
+                    System.out.println("danger");
+                } else {
+                    System.out.println("yes " + nums[left] + " " + nums[right]);
+                }
+            } else {
+                System.out.println("danger");
+            }
+
+            s = null;
+        }
     }
 }
