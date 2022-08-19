abed2052f75a6627377c2ce309df1141d716f028
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-08-20 00:26:58 +0900

    19082022

--- src/backjoon/b19082022.java
+++ src/backjoon/b19082022.java
@@ -3,7 +3,7 @@ package backjoon;
 import java.io.*;
 import java.util.*;
 
-//
+//입국심사
 //19082022
 
 public class b19082022 {
@@ -13,6 +13,43 @@ public class b19082022 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] times = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            times[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(times);
+
+        long left = 1;
+        long right = (long) times[n - 1] * m;
+
+        long answer = 0;
+
+        while (left <= right) {
+            long mid = (left + right) / 2;
+
+            long count = 0;
+
+            for (int i = 0; i < times.length; i++) {
+                count += mid / times[i];
+            }
+
+            if (count < m) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+                answer = mid;
+            }
+
+        }
+
+        System.out.println(answer);
+
     }
 }
