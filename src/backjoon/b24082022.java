05eb4f52b4ecff2a23eb1d0c04ef39f0287c5718
Mike-Jeong <alstlr65@gmail.com>
2022-08-25 00:48:04 +0900

    24082022

--- src/backjoon/b24082022.java
+++ src/backjoon/b24082022.java
@@ -3,7 +3,7 @@ package backjoon;
 import java.io.*;
 import java.util.*;
 
-//
+//꿀 따기 
 //24082022
 
 public class b24082022 {
@@ -13,7 +13,35 @@ public class b24082022 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-       
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] honey = new int[n + 1];
+        int[] sum = new int[n + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i < sum.length; i++) {
+            honey[i] = Integer.parseInt(st.nextToken());
+            sum[i] = sum[i - 1] + honey[i];
+        }
+
+        int max = 0;
+
+        for (int i = 2; i <= n - 1; i++) {
+            int ck = sum[n] - honey[1] - honey[i] + sum[n] - sum[i];
+            max = Math.max(max, ck);
+        }
+
+        for (int i = 2; i <= n - 1; i++) {
+            int ck = sum[n - 1] - sum[i - 1] + sum[i] - sum[1];
+            max = Math.max(max, ck);
+        }
+
+        for (int i = 2; i <= n - 1; i++) {
+            int ck = sum[n - 1] - honey[i] + sum[i - 1];
+            max = Math.max(max, ck);
+        }
+
+        System.out.println(max);
 
     }
 }
