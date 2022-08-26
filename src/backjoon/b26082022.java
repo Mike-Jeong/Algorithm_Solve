bd3f380d3174a074db3e2b37b94d17f9c70bc8f8
Mike-Jeong <alstlr65@gmail.com>
2022-08-27 00:19:26 +0900

    26082022

--- src/backjoon/b26082022.java
+++ src/backjoon/b26082022.java
@@ -3,7 +3,7 @@ package backjoon;
 import java.io.*;
 import java.util.*;
 
-//
+//강의실 배정
 //26082022
 
 public class b26082022 {
@@ -13,7 +13,55 @@ public class b26082022 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        
+        int n = Integer.parseInt(st.nextToken());
 
+        Lecture[] lecs = new Lecture[n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+
+            lecs[i] = new Lecture(s, e);
+        }
+
+        Arrays.sort(lecs);
+
+        Queue<Integer> queue = new PriorityQueue<>();
+        queue.add(lecs[0].e);
+
+        for (int i = 1; i < n; i++) {
+
+            if (queue.peek() <= lecs[i].s) {
+                queue.poll();
+            }
+
+            queue.add(lecs[i].e);
+        }
+
+        System.out.println(queue.size());
+
+    }
+}
+
+class Lecture implements Comparable<Lecture> {
+
+    int s;
+    int e;
+
+    public Lecture(int s, int e) {
+        this.s = s;
+        this.e = e;
+    }
+
+    @Override
+    public int compareTo(Lecture o) {
+
+        if (o.s == s) {
+            return e - o.e;
+        } else {
+            return s - o.s;
+        }
     }
 }
