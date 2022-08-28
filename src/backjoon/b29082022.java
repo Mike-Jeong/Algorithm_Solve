680520c6b7e4074247d7de517d53ad0f2aa1c91e
Mike-Jeong <alstlr65@gmail.com>
2022-08-29 06:33:36 +0900

    29082022

--- src/backjoon/b29082022.java
+++ src/backjoon/b29082022.java
@@ -1,9 +1,9 @@
 package backjoon;
 
 import java.io.*;
-import java.util.*;
+//import java.util.*;
 
-//
+//공통 부분 문자열
 //29082022
 
 public class b29082022 {
@@ -11,9 +11,24 @@ public class b29082022 {
     public static void main(String args[]) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        
+        String s1 = br.readLine();
+        String s2 = br.readLine();
+
+        int max = 0;
+        int[][] map = new int[s1.length() + 1][s2.length() + 1];
+
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    map[i][j] = map[i - 1][j - 1] + 1;
+                    max = Math.max(max, map[i][j]);
+                }
+            }
+        }
+
+        System.out.println(max);
+
     }
 
 }
