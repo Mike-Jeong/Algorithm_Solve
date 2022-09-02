89e6e85c490fe01a729d0f034ba48a9ed0925397
Mike-Jeong <alstlr65@gmail.com>
2022-09-02 15:44:47 +0900

    02092022

---
+++ src/backjoon/b02092022.java
@@ -0,0 +1,19 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//
+//02092022
+
+public class b02092022 {
+
+    public static void main(String args[]) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        
+
+    }
+}
