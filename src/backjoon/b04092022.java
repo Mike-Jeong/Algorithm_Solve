bf8cdeb42f99c7c97eba423153dbfa7895dec567
Mike-Jeong <alstlr65@gmail.com>
2022-09-05 22:30:16 +0900

    05092022

--- src/backjoon/b04092022.java
+++ src/backjoon/b04092022.java
@@ -3,16 +3,48 @@ package backjoon;
 import java.io.*;
 import java.util.*;
 
-//
+//이진 검색 트리
 //04092022
 
 public class b04092022 {
 
+    static ArrayList<Integer> tree;
+
     public static void main(String args[]) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        // StringTokenizer st = new StringTokenizer(br.readLine());
 
+        tree = new ArrayList<Integer>();
         
+        while (true) {
+            String n = br.readLine();
+            if (n == null || n.equals("")) {
+                break;
+            }
+
+            tree.add(Integer.parseInt(n));
+        }
+
+        postOrder(0, tree.size() - 1);
+
     }
+
+    public static void postOrder(int idx, int end) {
+        if (idx > end) {
+            return;
+        }
+
+        int mid = idx + 1;
+
+        while (mid <= end && tree.get(mid) < tree.get(idx)) {
+            mid++;
+        }
+
+        postOrder(idx + 1, mid - 1);
+        postOrder(mid, end);
+
+        System.out.println(tree.get(idx));
+    }
+
 }
---
+++ src/backjoon/b05092022.java
@@ -0,0 +1,20 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//
+//05092022
+
+public class b05092022 {
+
+    static ArrayList<Integer> tree;
+
+    public static void main(String args[]) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+    }
+}
