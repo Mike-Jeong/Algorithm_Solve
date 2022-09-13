f97bf107a73b252077a57157e95f34f71a2411a6
Mike-Jeong <alstlr65@gmail.com>
2022-09-13 18:28:35 +0900

    13092022

---
+++ src/backjoon/b13092022.java
@@ -0,0 +1,18 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//가장 긴 증가하는 부분 수열 4
+//13092022
+
+public class b13092022 {
+
+	public static void main(String args[]) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		
+	}
+}
