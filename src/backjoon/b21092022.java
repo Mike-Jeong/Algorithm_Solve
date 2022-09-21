d73f8abe9d77cce80dd5758850584d0bdd54113e
Mike-Jeong <alstlr65@gmail.com>
2022-09-22 02:52:09 +0900

    21092022

--- src/backjoon/b21092022.java
+++ src/backjoon/b21092022.java
@@ -18,6 +18,37 @@ public class b21092022 {
 		st = new StringTokenizer(br.readLine());
 		int k = Integer.parseInt(st.nextToken());
 
-		
+		if (k == (n * n)) {
+			System.out.println(n * n);
+		} else {
+			int left = 1;
+			int right = k;
+
+			while (left < right) {
+
+				int mid = (left + right) / 2;
+				int count = 0;
+
+				for (int i = 1; i <= n; i++) {
+					for (int j = 1; j <= n; j++) {
+						if (i * j <= mid) {
+							count++;
+						} else {
+							break;
+						}
+					}
+				}
+
+				System.out.println(count);
+
+				if (count < k) {
+					left = mid + 1;
+				} else {
+					right = mid;
+				}
+			}
+
+			System.out.println(left);
+		}
 	}
 }
