6b651d6954b140e3b4c022233ef33602fafbeacb
Mike-Jeong <alstlr65@gmail.com>
2022-11-01 22:56:00 +0900

    01112022

--- src/backjoon/b01112022_02.java
+++ src/backjoon/b01112022_02.java
@@ -6,6 +6,96 @@ import java.util.*;
 //가장 긴 바이토닉 부분 수열 (다시 풀기)
 //01112022
 
+//풀이과정 2
+//시간 복잡도 O(nlogn)
+
+public class b01112022_02 {
+
+	public static void main(String args[]) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+
+		int[] nums = new int[n];
+		int[] dp = new int[n];
+		int[] dp_min = new int[n];
+
+		List<Integer> list = new ArrayList<>();
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+
+			nums[i] = Integer.parseInt(st.nextToken());
+
+			if (i == 0) {
+				list.add(nums[i]);
+				continue;
+			}
+
+			if (list.get(list.size() - 1) < nums[i]) {
+				list.add(nums[i]);
+				dp[i] = list.size() - 1;
+			} else {
+				dp[i] = binarySearch(list, nums[i]);
+			}
+		}
+
+		list = new ArrayList<>();
+
+		for (int i = n - 1; i >= 0; i--) {
+
+			if (i == n - 1) {
+				list.add(nums[i]);
+				dp[i]++;
+				continue;
+			}
+
+			if (list.get(list.size() - 1) < nums[i]) {
+				list.add(nums[i]);
+				dp_min[i] = list.size() - 1;
+			} else {
+				dp_min[i] = binarySearch(list, nums[i]);
+			}
+
+			dp[i] = dp[i] + dp_min[i] + 1;
+		}
+
+		int ans = 0;
+
+		for (int i = 0; i < dp.length; i++) {
+			ans = Math.max(dp[i], ans);
+		}
+
+		System.out.println(ans);
+
+	}
+
+	public static int binarySearch(List<Integer> list, int num) {
+		int left = 0;
+		int right = list.size() - 1;
+
+		while (left < right) {
+			int mid = (left + right) / 2;
+
+			if (list.get(mid) >= num) {
+				right = mid;
+			} else {
+				left = mid + 1;
+			}
+		}
+		list.set(right, num);
+
+		return right;
+	}
+}
+
+
+// 풀이 과정 1
+//시간 복잡도 O(n^2)
+//공간 복잡도 O(n)
+/*
 public class b01112022_02 {
 
 	public static void main(String args[]) throws Exception {
@@ -55,4 +145,4 @@ public class b01112022_02 {
 
 	}
 }
-
+ */
