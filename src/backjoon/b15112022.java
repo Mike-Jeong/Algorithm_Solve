0fc7ec216811b71507ff7ee8ecf926d2de2dd489
Mike-Jeong <alstlr65@gmail.com>
2022-11-15 22:31:11 +0900

    15112022

---
+++ src/backjoon/b15112022.java
@@ -0,0 +1,33 @@
+package backjoon;
+
+import java.io.*;
+//import java.util.*;
+
+//한글 LCS
+//15112022
+
+public class b15112022 {
+
+	public static void main(String args[]) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// StringTokenizer st = new StringTokenizer(br.readLine());
+
+		String s1 = br.readLine();
+		String s2 = br.readLine();
+
+		int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+
+		for (int i = 1; i <= s1.length(); i++) {
+			for (int j = 1; j <= s2.length(); j++) {
+				if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+					dp[i][j] = dp[i - 1][j - 1] + 1;
+				} else {
+					dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+				}
+			}
+		}
+
+		System.out.println(dp[s1.length()][s2.length()]);
+	}
+}
