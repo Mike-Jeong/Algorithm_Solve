15afb293479f5f9cd0c97c9b25564e7149a90f66
Mike-Jeong <alstlr65@gmail.com>
2022-11-21 22:17:35 +0900

    20112022(나름의 수정안.. but fail)

--- src/backjoon/b20112022.java
+++ src/backjoon/b20112022.java
@@ -10,60 +10,93 @@ public class b20112022 {
 
 	static int n;
 	static int[][] map;
-	static boolean[][] checked;
-	static int[] dx = { 1, 0, -1, 0 };
-	static int[] dy = { 0, -1, 0, 1 };
+	static boolean[][] visited;
+	static List<Island> list = new LinkedList<>();
 
-	public static void main(String[] args) throws IOException {
+	static int[] rowOrder = new int[] { 0, 1, 0, -1 };
+	static int[] colOrder = new int[] { 1, 0, -1, 0 };
+
+	static int ans = -1;
+	static int maxLevel = -1;
+
+	public static void main(String args[]) throws Exception {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
+		n = Integer.parseInt(st.nextToken());
 		map = new int[n][n];
 
-		int maxHeight = 0;
 		for (int i = 0; i < n; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < n; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
-				if (map[i][j] > maxHeight) {
-					maxHeight = map[i][j];
-				}
+				list.add(new Island(i, j, map[i][j]));
+				maxLevel = Math.max(maxLevel, map[i][j]);
 			}
 		}
 
-		int max = 0;
+		Collections.sort(list, Collections.reverseOrder());
 
-		for (int height = 0; height < maxHeight + 1; height++) {
-			checked = new boolean[n][n];
-			int cnt = 0;
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < n; j++) {
+		while (maxLevel >= 0) {
 
-					if (!checked[i][j] && map[i][j] > height) {
-						cnt += dfs(i, j, height);
-					}
+			int count = 0;
+			visited = new boolean[n][n];
 
+			for (int i = 0; i < list.size(); i++) {
+
+				Island now = list.get(i);
+
+				if (now.level < maxLevel) {
+					break;
+				}
+
+				if (!visited[now.row][now.col]) {
+					//System.out.println(now.row + " " + now.col + " " + maxLevel);
+					recursion(now.row, now.col, maxLevel);
+					count++;
 				}
 			}
-			max = Math.max(max, cnt);
+
+			ans = Math.max(ans, count);
+			maxLevel--;
+
 		}
-		System.out.println(max);
+
+		//System.out.println(maxLevel);
+		System.out.println(ans);
 	}
 
-	static int dfs(int x, int y, int height) {
-		checked[x][y] = true;
-		for (int i = 0; i < 4; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
+	static void recursion(int row, int col, int level) {
+		visited[row][col] = true;
 
-			if (nx < 0 || ny < 0 || nx > n - 1 || ny > n - 1)
+		for (int i = 0; i < 4; i++) {
+			try {
+				if (map[row + rowOrder[i]][col + colOrder[i]] >= level
+						&& !visited[row + rowOrder[i]][col + colOrder[i]]) {
+					recursion(row + rowOrder[i], col + colOrder[i], level);
+				}
+			} catch (Exception e) {
 				continue;
-			if (checked[nx][ny])
-				continue;
-			if (map[nx][ny] > height) {
-				dfs(nx, ny, height);
 			}
 		}
-		return 1;
+	}
+}
+
+class Island implements Comparable<Island> {
+
+	int row;
+	int col;
+	int level;
+
+	public Island(int row, int col, int level) {
+		this.row = row;
+		this.col = col;
+		this.level = level;
+	}
+
+	@Override
+	public int compareTo(Island o) {
+		return level - o.level;
 	}
 }
