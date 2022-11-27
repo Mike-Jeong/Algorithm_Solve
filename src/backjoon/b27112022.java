8aed240419be9f9afc5b9fabff9ff850411e04a6
Mike-Jeong <alstlr65@gmail.com>
2022-11-28 00:08:09 +0900

    27112022_답안수정

--- src/backjoon/b27112022.java
+++ src/backjoon/b27112022.java
@@ -11,11 +11,10 @@ public class b27112022 {
 	public static void main(String args[]) throws Exception {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
-
 
 		Integer[] devices = new Integer[n];
 
@@ -26,17 +25,30 @@ public class b27112022 {
 
 		Arrays.sort(devices, Collections.reverseOrder());
 
-		PriorityQueue<Integer> powerSocket = new PriorityQueue<>();
+		if (n > m) {
+			PriorityQueue<Integer> powerSocket = new PriorityQueue<>();
 
-		for (int i = 0; i < n; i++) {
-			if (m > powerSocket.size()) {
+			for (int i = 0; i < m; i++) {
 				powerSocket.add(devices[i]);
-			} else {
-				powerSocket.add(devices[i] + powerSocket.poll());
 			}
-		}
 
-		System.out.println(powerSocket.toArray()[powerSocket.size() - 1]);
+			for (int i = m; i < n; i++) {
+
+				int finished = powerSocket.poll();
+				powerSocket.add(devices[i] + finished);
+			}
+
+			int ans = powerSocket.stream()
+			.mapToInt(x -> x)
+			.max()
+			.getAsInt();
+			System.out.println(ans);
+
+		} else {
+			System.out.println(devices[0]);
+		}
 
 	}
 }
+
+// 수정본
