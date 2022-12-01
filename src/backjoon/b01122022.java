5ddb79e853d9d419edc244d3499ea1090d344750
Mike-Jeong <alstlr65@gmail.com>
2022-12-01 23:40:00 +0900

    01122022

---
+++ src/backjoon/b01122022.java
@@ -0,0 +1,44 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//경로 찾기
+//01122022
+
+public class b01122022 {
+
+	public static void main(String args[]) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+
+		int[][] map = new int[n][n];
+
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < n; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				for (int j2 = 0; j2 < n; j2++) {
+					if (map[j][i] == 1 && map[i][j2] == 1) {
+						map[j][j2] = 1;
+					}
+				}
+			}
+		}
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print(map[i][j] + " ");
+			}
+			System.out.print("\n");
+		}
+	}
+}
