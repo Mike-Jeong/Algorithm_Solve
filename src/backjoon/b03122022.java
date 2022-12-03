5746ad2ab4cf5aaf2884eb3cbaa3c58db1da3366
Mike-Jeong <alstlr65@gmail.com>
2022-12-04 00:06:25 +0900

    03122022

--- src/backjoon/b03122022.java
+++ src/backjoon/b03122022.java
@@ -9,6 +9,7 @@ import java.util.*;
 public class b03122022 {
 
 	static char[][] map;
+
 	public static void main(String args[]) throws Exception {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,46 +26,37 @@ public class b03122022 {
 
 		recursion(0, 0, n);
 
+		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < map.length; i++) {
 			for (int j = 0; j < map.length; j++) {
-				System.out.print(map[i][j]);
+				sb.append(map[i][j]);
 			}
-			System.out.print("\n");
+			sb.append("\n");
 		}
+
+		System.out.println(sb);
 	}
 
 	static void recursion(int row, int col, int range) {
-
-		/*for (int i = 0; i < map.length; i++) {
-			for (int j = 0; j < map.length; j++) {
-				System.out.print(map[i][j]);
-			}
-			System.out.print("\n");
-		}
-
-		System.out.println("---------------------------------------");*/
-
-		
 
 		if (range == 1) {
 			return;
 		}
 
 		int count = 0;
-		for (int i = 0; i < map.length; i+=(range / 3)) {
-			for (int j = 0; j < map.length; j+=(range/3)) {
+		for (int i = 0; i < range; i += (range / 3)) {
+			for (int j = 0; j < range; j += (range / 3)) {
 
 				if (count == 4) {
-					
-					for (int j2 = 0; j2 < range/3; j2++) {
-						for (int k = 0; k < range/3; k++) {
-							System.out.println((j2 + i) + " " + (k + j) + " " + range/3);
-							map[j2 + i][k + j] = ' ';
+
+					for (int j2 = 0; j2 < range / 3; j2++) {
+						for (int k = 0; k < range / 3; k++) {
+							map[j2 + i + row][k + j + col] = ' ';
 						}
 					}
-					
+
 				} else {
-					recursion(i, j, range / 3);
+					recursion(i + row, j + col, range / 3);
 				}
 
 				count++;
@@ -72,37 +64,3 @@ public class b03122022 {
 		}
 	}
 }
-
-
-
-/* 
-
-***************************
-* ** ** ** ** ** ** ** ** *
-***************************
-***   ******   ******   ***
-* *   * ** *   * ** *   * *
-***   ******   ******   ***
-***************************
-* ** ** ** ** ** ** ** ** *
-***************************
-*********         *********
-* ** ** *         * ** ** *
-*********         *********
-***   ***         ***   ***
-* *   * *         * *   * *
-***   ***         ***   ***
-*********         *********
-* ** ** *         * ** ** *
-*********         *********
-***************************
-* ** ** ** ** ** ** ** ** *
-***************************
-***   ******   ******   ***
-* *   * ** *   * ** *   * *
-***   ******   ******   ***
-***************************
-* ** ** ** ** ** ** ** ** *
-***************************
-
-*/
